eea3cd680ab3b48f57780cd437269ac6b1b301dc
Mark Inderhees <mark.inderhees@gmail.com>
2020-01-12 15:11:28 -0800

    day 1 part 1 done

--- 2018/01/main.c
+++ 2018/01/main.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
+#include <stdint.h>
+#include "input.h"
+
+#define ARRAY_SIZE(x) (sizeof(x)/sizeof(x[0]))
 
 int main(int argc, char* argv[])
 {
-    printf("Hello World\n");
+    int32_t sum = 0;
+    for (uint32_t i = 0; i < ARRAY_SIZE(input); i++)
+    {
+        sum += input[i];
+    }
+
+    printf("%d\n", sum);
     return 0;
 }
