4848cab5d7d5f38818ab26a80253a33c538a314c
Mark Inderhees <mark.inderhees@gmail.com>
2020-01-13 21:12:30 -0800

    get checksum

---
+++ 2018/02/02.c
@@ -0,0 +1,64 @@
+#include "../common.h"
+#include "02input.h"
+
+uint32_t GetCharCount(char* string, char character)
+{
+    uint32_t i = 0;
+    uint32_t count = 0;
+    while (string[i] != '\0')
+    {
+        if (string[i] == character)
+        {
+            count++;
+        }
+        i++;
+    }
+
+    return count;
+}
+
+uint32_t GetChecksum(char* x[], uint32_t length)
+{
+    bool triedChar['z' - 'a' + 1];
+    memset(triedChar, 0, sizeof(triedChar));
+    uint32_t count2 = 0;
+    uint32_t count3 = 0;
+    bool counted2 = false;
+    bool counted3 = false;
+    for (uint32_t i = 0; i < length; i++)
+    {
+        char* string = x[i];
+        uint32_t j = 0;
+        while (string[j] != '\0')
+        {
+            char character = string[j];
+            if (!triedChar[character - 'a'])
+            {
+                triedChar[character - 'a'] = true;
+                uint32_t count = GetCharCount(string, character);
+                if (count == 2 && !counted2)
+                {
+                    counted2 = true;
+                    count2++;
+                }
+                else if (count == 3 && !counted3)
+                {
+                    counted3 = true;
+                    count3++;
+                }
+            }
+            j++;
+        }
+        memset(triedChar, 0, sizeof(triedChar));
+        counted2 = false;
+        counted3 = false;
+    }
+
+    return count2 * count3;
+}
+
+int main(int argc, char* argv[])
+{
+    printf("Checksum is %d\n", GetChecksum(input, ARRAY_SIZE(input)));
+    return 0;
+}
