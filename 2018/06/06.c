d01a8b34f9fa2e0a31a4dec0d6020c4cdc018699
Mark Inderhees <mark.inderhees@gmail.com>
2020-01-14 23:09:20 -0800

    find landing area close to everything

--- 2018/06/06.c
+++ 2018/06/06.c
@@ -39,53 +39,54 @@ uint32_t FindLargestArea(pair input[], uint32_t length)
     uint32_t width = maxX - minX + 1;
     uint32_t height = maxY - minY + 1;
     uint32_t size = width * height;
-    uint8_t* grid = malloc(size);
-    memset(grid, UINT8_MAX, size);
-    uint32_t* dotCount = malloc(sizeof(uint32_t) * length);
-    memset(dotCount, 0, sizeof(uint32_t) * length);
+    uint32_t* grid = malloc(size * sizeof(uint32_t));
+    memset(grid, 0, size * sizeof(uint32_t));
+    // uint32_t* dotCount = malloc(sizeof(uint32_t) * length);
+    // memset(dotCount, 0, sizeof(uint32_t) * length);
+    uint32_t someCount = 0; // Find count of dots less than 10,000
     for (uint32_t x = 0; x < width; x++)
     {
         for (uint32_t y = 0; y < height; y++)
         {
-            uint32_t minDistance = UINT32_MAX;
+            // uint32_t minDistance = UINT32_MAX;
             pair p = {x + minX, y + minY};
             for (uint32_t i = 0; i < length; i++)
             {
-                uint32_t d = FindDistance(p, input[i]);
-                if (d < minDistance)
-                {
-                    minDistance = d;
-                    grid[x + y * width] = i;
-                }
+                grid[x + y * width] += FindDistance(p, input[i]);
             }
-            dotCount[grid[x + y * width]]++;
+            // dotCount[grid[x + y * width]]++;
+            if (grid[x + y * width] < 10000)
+            {
+                someCount++;
+            }
         }
     }
 
-    // Erase anyone touching the edges
-    for (uint32_t x = 0; x < width; x++)
-    {
-        dotCount[grid[x + 0 * width]] = 0;
-        dotCount[grid[x + (height-1) * width]] = 0;
-    }
+    // // Erase anyone touching the edges
+    // for (uint32_t x = 0; x < width; x++)
+    // {
+    //     dotCount[grid[x + 0 * width]] = 0;
+    //     dotCount[grid[x + (height-1) * width]] = 0;
+    // }
 
-    for (uint32_t y = 0; y < height; y++)
-    {
-        dotCount[grid[0 + y * width]] = 0;
-        dotCount[grid[(width-1) + y * width]] = 0;
-    }
+    // for (uint32_t y = 0; y < height; y++)
+    // {
+    //     dotCount[grid[0 + y * width]] = 0;
+    //     dotCount[grid[(width-1) + y * width]] = 0;
+    // }
 
-    // Find largest count
-    uint32_t largestCount = 0;
-    for (uint32_t i = 0; i < length; i++)
-    {
-        if (dotCount[i] > largestCount)
-        {
-            largestCount = dotCount[i];
-        }
-    }
+    // // Find largest count
+    // uint32_t largestCount = 0;
+    // for (uint32_t i = 0; i < length; i++)
+    // {
+    //     if (dotCount[i] > largestCount)
+    //     {
+    //         largestCount = dotCount[i];
+    //     }
+    // }
 
-    return largestCount;
+    // return largestCount;
+    return someCount;
 }
 
 int main(int argc, char* argv[])
