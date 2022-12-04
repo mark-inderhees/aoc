92635373ecf45df30c31af2e62fe620fec62d538
Mark Inderhees <markind@fb.com>
2022-12-03 22:04:36 -0800

    day4 part 1

--- 2022/d04/src/main.rs
+++ 2022/d04/src/main.rs
@@ -5,13 +5,34 @@ use std::path::Path;
 fn main() {
     println!("Hello, world!");
 
-    if let Ok(lines) = read_lines("./src/input") {
+    let mut count = 0;
+    if let Ok(lines) = read_lines("./src/input.txt") {
         for line in lines {
             if let Ok(l) = line {
-                println!("{}", l);
+                let mut start = "";
+                let elves: Vec<&str> = l.split(",").collect();
+                let r1: Vec<&str> = elves[0].split("-").collect();
+                let r2: Vec<&str> = elves[1].split("-").collect();
+                let r1_0: u32 = r1[0].parse().unwrap();
+                let r1_1: u32 = r1[1].parse().unwrap();
+                let r2_0: u32 = r2[0].parse().unwrap();
+                let r2_1: u32 = r2[1].parse().unwrap();
+                // if r1[0] == r2[0] && r1[1] == r2[1] {
+                if r1_0 <= r2_0 && r1_1 >= r2_1 {
+                    count += 1;
+                    start = "*";
+                } else if r2_0 <= r1_0 && r2_1 >= r1_1 {
+                    count += 1;
+                    start = "*";
+                }
+                println!(
+                    "{} {}-{} {}-{} {} {}",
+                    l, r1_0, r1_1, r2_0, r2_1, count, start
+                );
             }
         }
     }
+    println!("count {}", count);
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
