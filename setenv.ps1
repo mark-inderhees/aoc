2a6e8bfa51680bd8e2f478a0a01800e0ae6e3338
Mark Inderhees <mark.inderhees@gmail.com>
2022-12-04 20:50:55 -0800

    some really old stuff

--- setenv.ps1
+++ setenv.ps1
@@ -1,2 +1,2 @@
 
-$env:Path = "C:\Program Files\GCC\bin;C:\Program Files\CMake\bin;C:\Program Files\Ninja;$env:Path"
+$env:Path = "C:\Program Files\LLVM\bin;C:\Program Files\GCC\bin;C:\Program Files\CMake\bin;C:\Program Files\Ninja;$env:Path"
