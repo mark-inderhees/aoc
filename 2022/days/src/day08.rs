eb8cda167c10f1e78d2413cef73607d30eca7560
Mark Inderhees <mark.inderhees@gmail.com>
2022-12-08 00:24:05 -0800

    day 8 clean up

--- 2022/days/src/day08.rs
+++ 2022/days/src/day08.rs
@@ -38,15 +38,15 @@ impl Puzzle for Day08 {
             for x in 1..(self.board.width() - 1) {
                 for direction in Direction::iterator() {
                     self.board.set_location(x, y);
-                    let height = self.board.get_current_value().clone();
-                    let mut heights = vec![];
-                    while let Some(height2) = self.board.step(direction) {
-                        heights.push(height2.clone());
+                    let tree_height = self.board.get_current_value().clone();
+                    let mut tree_heights = vec![];
+                    while let Some(tree_height2) = self.board.step(direction) {
+                        tree_heights.push(tree_height2.clone());
 
                     }
-                    let height_max = heights.iter().max().unwrap();
-                    let visible = height > *height_max;
-                    log::debug!("At {x},{y} going {direction:?}: {height} vs {height_max} = {visible}, {heights:?}");
+                    let tree_height_max = tree_heights.iter().max().unwrap().clone();
+                    let visible = tree_height > tree_height_max;
+                    log::debug!("At {x},{y} going {direction:?}: {tree_height} vs {tree_height_max} = {visible}, {tree_heights:?}");
                     if visible {
                         count += 1;
                         self.visible.set_at(x, y, 'v');
@@ -77,14 +77,11 @@ impl Puzzle for Day08 {
                 for direction in Direction::iterator() {
                     scores.push(0);
                     self.board.set_location(x, y);
-                    let height = self.board.get_current_value().clone();
-                    let mut heights = vec![];
-                    while let Some(height2) = self.board.step(direction) {
-                        heights.push(height2.clone());
-
+                    let tree_height = self.board.get_current_value().clone();
+                    while let Some(tree_height2) = self.board.step(direction) {
                         let s = scores.pop().unwrap().clone();
                         scores.push(s + 1);
-                        if *height2 >= height {
+                        if *tree_height2 >= tree_height {
                             break;
                         }
                     }
@@ -97,10 +94,10 @@ impl Puzzle for Day08 {
 
         log::debug!("Input Grid: {:#?}", self.board.grid());
         log::debug!("{:#?}", self.score.grid());
-        let part2 = self.score.grid().iter().max().unwrap();
-        log::info!("{}", part2);
+        let max = self.score.grid().iter().max().unwrap();
+        log::info!("{}", max);
 
-        Ok(part2.to_string())
+        Ok(max.to_string())
     }
 
     fn answer_part2(&mut self, test: bool) -> Option<String> {
