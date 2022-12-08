f0ceac9191460aeb4a7f0fee96b235120ba69caf
Mark Inderhees <mark.inderhees@gmail.com>
2022-12-07 23:56:54 -0800

    day 8 validate works

--- 2022/days/src/day08.rs
+++ 2022/days/src/day08.rs
@@ -100,18 +100,76 @@ impl Puzzle for Day08 {
 
         log::debug!("Input Grid: {:#?}", day.board);
 
-        let x_max: i32 = day.board.grid.size().0.try_into().unwrap();
-        let y_max: i32 = day.board.grid.size().1.try_into().unwrap();
+        Ok(day)
+    }
+
+    fn solve_part1(&mut self) -> Result<String> {
+        let x_max: i32 = self.board.grid.size().0.try_into().unwrap();
+        let y_max: i32 = self.board.grid.size().1.try_into().unwrap();
         let mut count = x_max * 2 + y_max * 2 - 4;
         for y in 1..(y_max - 1) {
             for x in 1..(x_max - 1) {
                 let mut scores = vec![];
                 for direction in Direction::iter() {
                     scores.push(0);
-                    day.board.set_location(x, y);
-                    let height = day.board.get_current_value().clone();
+                    self.board.set_location(x, y);
+                    let height = self.board.get_current_value().clone();
                     let mut heights = vec![];
-                    while let Some(height2) = day.board.step(direction) {
+                    while let Some(height2) = self.board.step(direction) {
+                        heights.push(height2.clone());
+
+                        let s = scores.pop().unwrap().clone();
+                        scores.push(s + 1);
+                        if *height2 >= height {
+                            // break; // part 2
+                        }
+                    }
+                    let height_max = heights.iter().max().unwrap();
+                    let visible = height > *height_max;
+                    // log::debug!("At {x},{y} going {direction:?}: {height} vs {height_max} = {visible}, {heights:?}");
+                    if visible {
+                        count += 1;
+                        let x_: usize = x.try_into().unwrap();
+                        let y_: usize = y.try_into().unwrap();
+                        self.visible.grid[y_][x_] = 'v';
+                        break;
+                    }
+                }
+                let mega_score = scores.iter().fold(1, |a, x| a * x);
+                log::debug!("At {x},{y} score {scores:?} --> {mega_score}");
+                let x_: usize = x.try_into().unwrap();
+                let y_: usize = y.try_into().unwrap();
+                self.score.grid[y_][x_] = mega_score;
+            }
+        }
+
+        log::debug!("Input Grid: {:#?}", self.board.grid);
+        log::debug!("{:#?}", self.visible.grid);
+        log::info!("{count}");
+
+        Ok(count.to_string())
+    }
+
+    fn answer_part1(&mut self, test: bool) -> Option<String> {
+        match test {
+            true => Some(21.to_string()),
+            false => Some(1698.to_string()),
+        }
+    }
+
+    fn solve_part2(&mut self) -> Result<String> {
+        let x_max: i32 = self.board.grid.size().0.try_into().unwrap();
+        let y_max: i32 = self.board.grid.size().1.try_into().unwrap();
+        let mut count = x_max * 2 + y_max * 2 - 4;
+        for y in 1..(y_max - 1) {
+            for x in 1..(x_max - 1) {
+                let mut scores = vec![];
+                for direction in Direction::iter() {
+                    scores.push(0);
+                    self.board.set_location(x, y);
+                    let height = self.board.get_current_value().clone();
+                    let mut heights = vec![];
+                    while let Some(height2) = self.board.step(direction) {
                         heights.push(height2.clone());
 
                         let s = scores.pop().unwrap().clone();
@@ -127,7 +185,7 @@ impl Puzzle for Day08 {
                         count += 1;
                         let x_: usize = x.try_into().unwrap();
                         let y_: usize = y.try_into().unwrap();
-                        day.visible.grid[y_][x_] = 'v';
+                        self.visible.grid[y_][x_] = 'v';
                         // break; part 2
                     }
                 }
@@ -135,38 +193,24 @@ impl Puzzle for Day08 {
                 log::debug!("At {x},{y} score {scores:?} --> {mega_score}");
                 let x_: usize = x.try_into().unwrap();
                 let y_: usize = y.try_into().unwrap();
-                day.score.grid[y_][x_] = mega_score;
+                self.score.grid[y_][x_] = mega_score;
             }
         }
 
-        log::debug!("Input Grid: {:#?}", day.board.grid);
-        // log::debug!("{:#?}", day.visible.grid);
-        log::debug!("{:#?}", day.score.grid);
+        log::debug!("Input Grid: {:#?}", self.board.grid);
+        // log::debug!("{:#?}", self.visible.grid);
+        log::debug!("{:#?}", self.score.grid);
         log::info!("{count}");
-        log::info!("{}", day.score.grid.iter().max().unwrap());
+        let part2 = self.score.grid.iter().max().unwrap();
+        log::info!("{}", part2);
 
-        Ok(day)
-    }
-
-    fn solve_part1(&mut self) -> Result<String> {
-        Ok("to do".to_string())
-    }
-
-    fn answer_part1(&mut self, test: bool) -> Option<String> {
-        match test {
-            true => None,
-            false => None,
-        }
-    }
-
-    fn solve_part2(&mut self) -> Result<String> {
-        Ok("to do".to_string())
+        Ok(part2.to_string())
     }
 
     fn answer_part2(&mut self, test: bool) -> Option<String> {
         match test {
-            true => None,
-            false => None,
+            true => Some(8.to_string()),
+            false => Some(672280.to_string()),
         }
     }
 }
