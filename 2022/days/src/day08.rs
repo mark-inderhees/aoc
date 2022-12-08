ec5ec2bda3332111d4fc296cd689512e39f0a239
Mark Inderhees <mark.inderhees@gmail.com>
2022-12-08 00:12:14 -0800

    day 8 little cleanup

--- 2022/days/src/day08.rs
+++ 2022/days/src/day08.rs
@@ -37,33 +37,23 @@ impl Puzzle for Day08 {
         let mut count = self.board.width() * 2 + self.board.height() * 2 - 4;
         for y in 1..(self.board.height() - 1) {
             for x in 1..(self.board.width() - 1) {
-                let mut scores = vec![];
                 for direction in Direction::iter() {
-                    scores.push(0);
                     self.board.set_location(x, y);
                     let height = self.board.get_current_value().clone();
                     let mut heights = vec![];
                     while let Some(height2) = self.board.step(direction) {
                         heights.push(height2.clone());
 
-                        let s = scores.pop().unwrap().clone();
-                        scores.push(s + 1);
-                        if *height2 >= height {
-                            // break; // part 2
-                        }
                     }
                     let height_max = heights.iter().max().unwrap();
                     let visible = height > *height_max;
-                    // log::debug!("At {x},{y} going {direction:?}: {height} vs {height_max} = {visible}, {heights:?}");
+                    log::debug!("At {x},{y} going {direction:?}: {height} vs {height_max} = {visible}, {heights:?}");
                     if visible {
                         count += 1;
                         self.visible.set_at(x, y, 'v');
                         break;
                     }
                 }
-                let mega_score = scores.iter().fold(1, |a, x| a * x);
-                log::debug!("At {x},{y} score {scores:?} --> {mega_score}");
-                self.score.set_at(x, y, mega_score);
             }
         }
 
@@ -82,7 +72,6 @@ impl Puzzle for Day08 {
     }
 
     fn solve_part2(&mut self) -> Result<String> {
-        let mut count = self.board.width() * 2 + self.board.height() * 2 - 4;
         for y in 1..(self.board.height() - 1) {
             for x in 1..(self.board.width() - 1) {
                 let mut scores = vec![];
@@ -97,16 +86,8 @@ impl Puzzle for Day08 {
                         let s = scores.pop().unwrap().clone();
                         scores.push(s + 1);
                         if *height2 >= height {
-                            break; // part 2
+                            break;
                         }
-                    }
-                    let height_max = heights.iter().max().unwrap();
-                    let visible = height > *height_max;
-                    // log::debug!("At {x},{y} going {direction:?}: {height} vs {height_max} = {visible}, {heights:?}");
-                    if visible {
-                        count += 1;
-                        self.visible.set_at(x, y, 'v');
-                        // break; part 2
                     }
                 }
                 let mega_score = scores.iter().fold(1, |a, x| a * x);
@@ -117,7 +98,6 @@ impl Puzzle for Day08 {
 
         log::debug!("Input Grid: {:#?}", self.board.grid);
         log::debug!("{:#?}", self.score.grid);
-        log::info!("{count}");
         let part2 = self.score.grid.iter().max().unwrap();
         log::info!("{}", part2);
 
