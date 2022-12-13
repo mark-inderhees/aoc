876a27b335d69646f3eff7b28a1043c43c4925f4
Mark Inderhees <mark.inderhees@gmail.com>
2022-12-12 20:20:57 -0800

    optimize a little

--- rust/src/utils/board.rs
+++ rust/src/utils/board.rs
@@ -195,6 +195,7 @@ where
         false
     }
 
+    #[allow(dead_code)]
     pub fn get_nearby_squares(&mut self, player: PlayerId) -> Vec<Direction> {
         let mut values = vec![];
         let orig_point = self.get_player_location(player);
@@ -295,37 +296,35 @@ where
             }
             self.grid_state[y][x].step_count = job.count;
 
-            // Force current location
-            self.set_player_location(from_player, job.location);
-            let my_char = self.get_player_value(from_player);
-
             // Try all new locations
-            let directions = self.get_nearby_squares(from_player);
-            for direction in directions {
-                // Try this location
+            for direction in Direction::straight_iterator() {
+                // Force current location
                 self.set_player_location(from_player, job.location);
-                self.step_player(from_player, direction).unwrap();
-                let new_location = self.get_player_location(from_player);
-                let near_char = self.get_player_value(from_player);
+                let my_char = self.get_player_value(from_player);
 
-                // See if we are allowed to move here
-                if valid_move(my_char, near_char) {
-                    // Check if we are done
-                    let taget = self.get_player_location(taget_player);
-                    if new_location.x == taget.x && new_location.y == taget.y {
-                        log::debug!("THIS IS THE END = {}", job.count);
-                        let final_count = job.count + 1;
-                        if final_count < shortest_path {
-                            shortest_path = final_count;
+                // Try this move
+                if let Some(near_char) = self.step_player(from_player, direction) {
+                    let new_location = self.get_player_location(from_player);
+
+                    // See if we are allowed to move here
+                    if valid_move(my_char, near_char) {
+                        // Check if we are done
+                        let taget = self.get_player_location(taget_player);
+                        if new_location.x == taget.x && new_location.y == taget.y {
+                            log::debug!("THIS IS THE END = {}", job.count);
+                            let final_count = job.count + 1;
+                            if final_count < shortest_path {
+                                shortest_path = final_count;
+                            }
+                            continue;
                         }
-                        continue;
-                    }
 
-                    // We can move, so do it!
-                    jobs.push(PathWork {
-                        location: new_location,
-                        count: job.count + 1,
-                    });
+                        // We can move, so do it!
+                        jobs.push(PathWork {
+                            location: new_location,
+                            count: job.count + 1,
+                        });
+                    }
                 }
             }
         }
