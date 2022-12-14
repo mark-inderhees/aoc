0d998118b7ed2cde011377df9b7cf674d294c7f9
Mark Inderhees <mark.inderhees@gmail.com>
2022-12-13 23:42:28 -0800

    day 14 part 1

--- rust/src/utils/board.rs
+++ rust/src/utils/board.rs
@@ -12,9 +12,7 @@ pub const INVALID_PLAYER: usize = usize::MAX;
 #[derive(Debug, Clone, Copy)]
 struct Player<T>
 where
-    T: Clone,
-    T: Copy,
-    T: Debug,
+    T: Clone + Copy + Debug + PartialEq + std::fmt::Display,
 {
     point: BoardPoint,
     id: T,
@@ -29,13 +27,13 @@ struct State {
 #[derive(Debug)]
 pub struct Board<T>
 where
-    T: Clone,
-    T: Copy,
-    T: Debug,
+    T: Clone + Copy + Debug + PartialEq + std::fmt::Display,
 {
     grid: Grid<T>,
     grid_state: Grid<State>,
     players: Vec<Player<T>>,
+    walls: Vec<T>,
+    players_are_walls: bool,
 }
 
 #[derive(Debug, EnumIter, Clone, Copy, PartialEq)]
@@ -68,15 +66,15 @@ impl Direction {
 
 impl<T> Board<T>
 where
-    T: Clone,
-    T: Copy,
-    T: Debug,
+    T: Clone + Copy + Debug + PartialEq + std::fmt::Display,
 {
     pub fn new() -> Board<T> {
         Board {
             grid: grid![],
             grid_state: grid![],
             players: vec![],
+            walls: vec![],
+            players_are_walls: false,
         }
     }
 
@@ -101,6 +99,24 @@ where
     pub fn add_player(&mut self, point: BoardPoint, id: T) -> PlayerId {
         self.players.push(Player { point, id });
         self.players.len() - 1
+    }
+
+    pub fn add_wall(&mut self, wall: T) {
+        self.walls.push(wall);
+    }
+
+    pub fn set_players_as_walls(&mut self) {
+        self.players_are_walls = true;
+    }
+
+    pub fn player_is_here(&self, location: BoardPoint) -> bool {
+        for player in &self.players {
+            if player.point == location {
+                return true;
+            }
+        }
+
+        false
     }
 
     pub fn width(&self) -> i32 {
@@ -174,10 +190,17 @@ where
             _ if new_location.point.x == x_max => None,
             _ if new_location.point.y == y_max => None,
             _ => {
-                self.players[player] = new_location;
                 let x: usize = new_location.point.x as usize;
                 let y: usize = new_location.point.y as usize;
-                Some(self.grid[y][x])
+                let value = self.grid[y][x];
+                if self.walls.contains(&value) {
+                    return None;
+                }
+                if self.players_are_walls && self.player_is_here(new_location.point) {
+                    return None;
+                }
+                self.players[player] = new_location;
+                Some(value)
             }
         }
     }
@@ -246,6 +269,14 @@ where
     }
 
     pub fn print_board_with_players(&mut self) {
+        self.print_board_with_players_helper(false);
+    }
+
+    pub fn print_board_with_players_pretty(&mut self) {
+        self.print_board_with_players_helper(true);
+    }
+
+    fn print_board_with_players_helper(&mut self, pretty: bool) {
         let orig: Vec<T> = self
             .players
             .iter()
@@ -255,7 +286,16 @@ where
         for player in self.players.clone().iter().rev() {
             self.set_at(player.point, player.id);
         }
-        log::debug!("{:#?}", self.grid);
+        if pretty {
+            for row in 0..self.height() as usize {
+                for t in self.grid.iter_row(row) {
+                    print!("{}", t);
+                }
+                println!("");
+            }
+        } else {
+            log::debug!("{:#?}", self.grid);
+        }
         for (player, id) in zip(self.players.clone(), orig) {
             self.set_at(player.point, id);
         }
--- rust/src/year2022/day14.rs
+++ rust/src/year2022/day14.rs
@@ -1,29 +1,177 @@
 use anyhow::Result;
 
 use crate::puzzle::Puzzle;
+use crate::utils::board::*;
 
 #[allow(unused_imports)]
 use crate::utils::utils::*;
 
-pub struct Day14 {}
+pub struct Day14 {
+    grid: Board<char>,
+    offset: BoardPoint,
+}
+
+fn drop_sand(day: &mut Day14) -> u32 {
+    let origin = BoardPoint {
+        x: 500 - day.offset.x,
+        y: 0,
+    };
+    let directions = vec![Direction::Down, Direction::DownLeft, Direction::DownRight];
+    let mut count = 0;
+
+    loop {
+        let id = day.grid.add_player(origin, 'o');
+        let mut okay;
+        loop {
+            okay = false;
+            for direction in directions.iter() {
+                if day.grid.step_player(id, *direction).is_some() {
+                    okay = true;
+                    break;
+                }
+            }
+            if !okay {
+                // Player can no longer move
+                break;
+            }
+        }
+        if !okay {
+            // Check if player fell off
+            let location = day.grid.get_player_location(id);
+            if location.x == 0
+                || location.x == day.grid.width() - 1
+                || location.y == day.grid.height() - 1
+            {
+                break;
+            }
+        }
+        count += 1;
+    }
+
+    count
+}
 
 impl Puzzle for Day14 {
     #[allow(unused_variables)]
     fn from_input(input: &str) -> Result<Self> {
         #[allow(unused_mut)]
-        let mut day = Day14 {};
+        let mut day = Day14 {
+            grid: Board::new(),
+            offset: BoardPoint { x: 0, y: 0 },
+        };
+
+        let test = input.lines().count() < 10;
+        if test {
+            // 11x11 grid
+            // min, max: Point { x: 494, y: 4 }, Point { x: 503, y: 9 }
+            day.offset.x = 493;
+            let row = vec!['.'; 11];
+            for _ in 0..11 {
+                day.grid.push_row(row.clone());
+            }
+        } else {
+            // min, max: Point { x: 483, y: 16 }, Point { x: 544, y: 164 }
+            day.offset.x = 480;
+            let row = vec!['.'; 100];
+            for _ in 0..200 {
+                day.grid.push_row(row.clone());
+            }
+        }
+
+        // Add wall types
+        day.grid.add_wall('#');
+        day.grid.set_players_as_walls();
+
+        let mut p_max = BoardPoint { x: 0, y: 0 };
+        let mut p_min = BoardPoint { x: 1000, y: 1000 };
+
+        for line in input.lines() {
+            let mut started = false;
+            let mut first = BoardPoint { x: 0, y: 0 };
+            for point_str in line.split("->") {
+                let values: Vec<i32> = get_vals(point_str);
+                let second = BoardPoint {
+                    x: values[0],
+                    y: values[1],
+                };
+                if second.x > p_max.x {
+                    p_max.x = second.x;
+                }
+                if second.x < p_min.x {
+                    p_min.x = second.x;
+                }
+                if second.y > p_max.y {
+                    p_max.y = second.y;
+                }
+                if second.y < p_min.y {
+                    p_min.y = second.y;
+                }
+                if started {
+                    // connect lines
+                    let mut begin = BoardPoint {
+                        x: first.x,
+                        y: first.y,
+                    };
+                    let mut end = BoardPoint {
+                        x: second.x,
+                        y: second.y,
+                    };
+                    if first.x == second.x {
+                        if first.y > second.y {
+                            begin.y = second.y;
+                            end.y = first.y;
+                        }
+                        for y in begin.y..=end.y {
+                            day.grid.set_at(
+                                BoardPoint {
+                                    x: begin.x - day.offset.x,
+                                    y,
+                                },
+                                '#',
+                            );
+                        }
+                    } else if first.y == second.y {
+                        if first.x > second.x {
+                            begin.x = second.x;
+                            end.x = first.x;
+                        }
+                        for x in begin.x..=end.x {
+                            day.grid.set_at(
+                                BoardPoint {
+                                    x: x - day.offset.x,
+                                    y: begin.y,
+                                },
+                                '#',
+                            );
+                        }
+                    } else {
+                        panic!("Unexpected line connection");
+                    }
+                }
+                started = true;
+                first.x = second.x;
+                first.y = second.y;
+            }
+        }
+
+        println!("{:?}, {:?}", p_min, p_max);
+        day.grid.print_board_with_players_pretty();
 
         Ok(day)
     }
 
     fn solve_part1(&mut self) -> Result<String> {
-        Ok("to do".to_string())
+        let count = drop_sand(self);
+        println!("{count}");
+        self.grid.print_board_with_players_pretty();
+
+        Ok(count.to_string())
     }
 
     fn answer_part1(&mut self, test: bool) -> Option<String> {
         match test {
-            true => None,
-            false => None,
+            true => Some(24.to_string()),
+            false => Some(614.to_string()),
         }
     }
 
