6faa3207977512bf0700cb6d95f542ac4740d282
Mark Inderhees <markind@fb.com>
2022-12-21 11:24:09 -0800

    begin day 21 cleanup

--- rust/src/year2022/day21.rs
+++ rust/src/year2022/day21.rs
@@ -1,6 +1,5 @@
-use std::collections::HashMap;
-
 use anyhow::Result;
+use std::collections::HashMap;
 
 use crate::puzzle::Puzzle;
 
@@ -25,8 +24,8 @@ struct Monkey {
     value: Option<i128>,
     id: String,
     operator: Operator,
-    part1: String,
-    part2: String,
+    left: String,
+    right: String,
     try_count: u32,
 }
 
@@ -51,8 +50,8 @@ fn do_math(
             Operator::Multiply => key / value2,
             Operator::Divide => {
                 if i_am_part_1 {
-                    return key * value2
-                }else {
+                    return key * value2;
+                } else {
                     return value2 / key;
                 }
             }
@@ -93,26 +92,32 @@ fn populate(day: &mut Day21, start_id: &str, invert_operations: bool) {
             // We have work to do, check if operators are available, else schedule work
             let mut new_jobs = vec![job.clone()];
 
-            if monkey.part1 == "" || monkey.part2 == "" {
+            if monkey.left == "" || monkey.right == "" {
                 continue;
             }
 
-            let monkey1 = &day.monkeys[&monkey.part1];
-            let monkey2 = &day.monkeys[&monkey.part2];
+            let monkey_left = &day.monkeys[&monkey.left];
+            let monkey_right = &day.monkeys[&monkey.right];
 
             if monkey.try_count > 2 {
                 continue;
             }
 
-            if let (Some(value1), Some(value2)) = (monkey1.value, monkey2.value) {
+            if let (Some(value1), Some(value2)) = (monkey_left.value, monkey_right.value) {
                 // We can comput now, so do it!
-                monkey.value = Some(do_math(monkey.operator, value1, value2, invert_operations, false));
+                monkey.value = Some(do_math(
+                    monkey.operator,
+                    value1,
+                    value2,
+                    invert_operations,
+                    false,
+                ));
                 log::debug!(
                     "Doing math {} = {} {:?} {}, {} {:?} {} = {}",
                     monkey.id,
-                    monkey1.id,
+                    monkey_left.id,
                     monkey.operator,
-                    monkey2.id,
+                    monkey_right.id,
                     value1,
                     monkey.operator,
                     value2,
@@ -121,16 +126,17 @@ fn populate(day: &mut Day21, start_id: &str, invert_operations: bool) {
                 day.monkeys.insert(job.id.clone(), monkey);
                 continue;
             }
-            if monkey2.value.is_none() {
+            if monkey_left.value.is_none() {
                 new_jobs.push(Work {
-                    id: monkey2.id.clone(),
+                    id: monkey_left.id.clone(),
                 });
             }
-            if monkey1.value.is_none() {
+            if monkey_right.value.is_none() {
                 new_jobs.push(Work {
-                    id: monkey1.id.clone(),
+                    id: monkey_right.id.clone(),
                 });
             }
+
             jobs.extend(new_jobs);
         }
     }
@@ -138,18 +144,18 @@ fn populate(day: &mut Day21, start_id: &str, invert_operations: bool) {
 
 fn find_monkeys(day: &Day21, my_id: &str) -> (String, String, Operator, bool) {
     // Search the hash map, find the monkey that depends on me
-    for (id1, monkey) in day.monkeys.iter() {
-        if monkey.part1 == my_id {
+    for (key, monkey) in day.monkeys.iter() {
+        if monkey.left == my_id {
             return (
-                id1.to_string(),
-                monkey.part2.to_string(),
+                key.to_string(),
+                monkey.right.to_string(),
                 monkey.operator,
                 true,
             );
-        } else if monkey.part2 == my_id {
+        } else if monkey.right == my_id {
             return (
-                id1.to_string(),
-                monkey.part1.to_string(),
+                key.to_string(),
+                monkey.left.to_string(),
                 monkey.operator,
                 false,
             );
@@ -256,8 +262,8 @@ impl Puzzle for Day21 {
                 continue;
             }
             data = data[1].trim().split(" ").collect();
-            monkey.part1 = data[0].to_string();
-            monkey.part2 = data[2].to_string();
+            monkey.left = data[0].to_string();
+            monkey.right = data[2].to_string();
             day.monkeys.insert(monkey.id.clone(), monkey);
         }
 
@@ -279,10 +285,10 @@ impl Puzzle for Day21 {
     }
 
     fn solve_part2(&mut self) -> Result<String> {
-        // Side 1 has human for my test and real input
+        // Left side has human for my test and real input
         let root = &self.monkeys["root"];
-        let id1 = root.part1.clone();
-        let id2 = root.part2.clone();
+        let id_left = root.left.clone();
+        let id_right = root.right.clone();
 
         // Make sure human is none
         let mut human = self.monkeys["humn"].clone();
@@ -290,18 +296,18 @@ impl Puzzle for Day21 {
         self.monkeys.insert("humn".to_string(), human);
 
         // Solve side 2 first
-        log::debug!("Populate side 2, find {}", id2);
-        populate(self, &id2, false);
+        log::debug!("Populate side 2, find {}", id_right);
+        populate(self, &id_right, false);
 
-        log::debug!("Trying to pre populate side 1, find {}", id1);
-        populate(self, &id1, false);
+        log::debug!("Trying to pre populate side 1, find {}", id_left);
+        populate(self, &id_left, false);
 
         // Now we know what monkey on side 1 value should be
-        let mut monkey1 = self.monkeys[&id1].clone();
-        let value = self.monkeys[&id2].value.unwrap();
+        let mut monkey1 = self.monkeys[&id_left].clone();
+        let value = self.monkeys[&id_right].value.unwrap();
         log::debug!("Found value {value} for root");
         monkey1.value = Some(value);
-        self.monkeys.insert(id1.clone(), monkey1);
+        self.monkeys.insert(id_left.clone(), monkey1);
 
         // Now use reverse logic to find human value
         log::debug!("Solve for human using inverse logic");
