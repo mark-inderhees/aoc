d6241b5a1b9d563a7863f8baf31ac51ec8ace9c6
Mark Inderhees <mark.inderhees@gmail.com>
2022-12-20 22:51:10 -0800

    day 21 part 1

--- rust/src/year2022/day21.rs
+++ rust/src/year2022/day21.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use anyhow::Result;
 
 use crate::puzzle::Puzzle;
@@ -5,25 +7,138 @@ use crate::puzzle::Puzzle;
 #[allow(unused_imports)]
 use crate::utils::utils::*;
 
-pub struct Day21 {}
+pub struct Day21 {
+    monkeys: HashMap<String, Monkey>,
+}
+
+#[derive(Default, Debug, Clone, Copy)]
+enum Operator {
+    #[default]
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
+}
+
+#[derive(Default, Debug, Clone)]
+struct Monkey {
+    value: Option<i128>,
+    id: String,
+    operator: Operator,
+    part1: String,
+    part2: String,
+}
+
+fn do_math(operator: Operator, value1: i128, value2: i128) -> i128 {
+    match operator {
+        Operator::Add => value1 + value2,
+        Operator::Subtract => value1 - value2,
+        Operator::Multiply => value1 * value2,
+        Operator::Divide => value1 / value2,
+    }
+}
+
+fn populate(day: &mut Day21) {
+    #[derive(Clone)]
+    struct Work {
+        id: String,
+    }
+    let job = Work {
+        id: "root".to_string(),
+    };
+    let mut jobs = vec![job];
+
+    while jobs.len() > 0 {
+        let job = jobs.pop().unwrap();
+        let mut monkey = day.monkeys[&job.id].clone();
+
+        if let Some(_value) = monkey.value {
+            panic!("Not expected populated value");
+        } else {
+            // We have work to do, check if operators are available, else schedule work
+            let mut new_jobs = vec![job.clone()];
+
+            let monkey1 = &day.monkeys[&monkey.part1];
+            let monkey2 = &day.monkeys[&monkey.part2];
+
+            if let (Some(value1), Some(value2)) = (monkey1.value, monkey2.value) {
+                // We can comput now, so do it!
+                monkey.value = Some(do_math(monkey.operator, value1, value2));
+                log::debug!(
+                    "Doing math {} = {} {:?} {}, {} {:?} {} = {}",
+                    monkey.id,
+                    monkey1.id,
+                    monkey.operator,
+                    monkey2.id,
+                    value1,
+                    monkey.operator,
+                    value2,
+                    monkey.value.unwrap()
+                );
+                day.monkeys.insert(job.id.clone(), monkey);
+                continue;
+            } else if monkey1.value.is_none() {
+                new_jobs.push(Work {
+                    id: monkey1.id.clone(),
+                });
+            } else if monkey2.value.is_none() {
+                new_jobs.push(Work {
+                    id: monkey2.id.clone(),
+                });
+            }
+            jobs.extend(new_jobs);
+        }
+    }
+}
 
 impl Puzzle for Day21 {
     #[allow(unused_variables)]
     fn from_input(input: &str) -> Result<Self> {
         #[allow(unused_mut)]
-        let mut day = Day21 {};
+        let mut day = Day21 {
+            monkeys: HashMap::new(),
+        };
+
+        for line in input.lines() {
+            let mut monkey = Monkey {
+                ..Default::default()
+            };
+            let mut data: Vec<&str> = line.split(":").collect();
+            monkey.id = data[0].to_string();
+            let str = data[1].to_string();
+            if str.contains("+") {
+                monkey.operator = Operator::Add;
+            } else if str.contains("-") {
+                monkey.operator = Operator::Subtract;
+            } else if str.contains("*") {
+                monkey.operator = Operator::Multiply;
+            } else if str.contains("/") {
+                monkey.operator = Operator::Divide;
+            } else {
+                monkey.value = Some(get_val(&str));
+                day.monkeys.insert(monkey.id.clone(), monkey);
+                continue;
+            }
+            data = data[1].trim().split(" ").collect();
+            monkey.part1 = data[0].to_string();
+            monkey.part2 = data[2].to_string();
+            day.monkeys.insert(monkey.id.clone(), monkey);
+        }
 
         Ok(day)
     }
 
     fn solve_part1(&mut self) -> Result<String> {
-        Ok("to do".to_string())
+        populate(self);
+        log::debug!("Monkies {:#?}", self.monkeys);
+        let answer = self.monkeys["root"].value.unwrap();
+        Ok(answer.to_string())
     }
 
     fn answer_part1(&mut self, test: bool) -> Option<String> {
         match test {
-            true => None,
-            false => None,
+            true => Some(152.to_string()),
+            false => Some(80326079210554u64.to_string()),
         }
     }
 
