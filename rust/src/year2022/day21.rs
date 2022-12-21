b5c17b1645db74ca43b5f18c687d33d4d718e566
Mark Inderhees <markind@fb.com>
2022-12-21 13:18:23 -0800

    day 21 in the book

--- rust/src/year2022/day21.rs
+++ rust/src/year2022/day21.rs
@@ -20,6 +20,8 @@ struct Monkey {
     try_count: u32,
 }
 
+// Walk a tree of math equations, starting at the root. If an equation cannot
+// be solved yet, then move to the child nodes to try and solve that equation.
 fn populate(day: &mut Day21, start_id: &str) {
     #[derive(Clone)]
     struct Work {
@@ -30,50 +32,58 @@ fn populate(day: &mut Day21, start_id: &str) {
     };
     let mut jobs = vec![job];
 
+    // Utilize try count to opportunistically fail if a branch is currently unsolvable
     for monkey in day.monkeys.values_mut() {
         monkey.try_count = 0;
     }
 
     while jobs.len() > 0 {
+        // Get the job to do
         let job = jobs.pop().unwrap();
         let mut monkey = day.monkeys[&job.id].clone();
         monkey.try_count += 1;
         day.monkeys.insert(job.id.clone(), monkey.clone());
 
+        // Look at the job's value
         if let Some(_value) = monkey.value {
             panic!("Not expected populated value");
         } else {
-            // We have work to do, check if operators are available, else schedule work
-            let mut new_jobs = vec![job.clone()];
+            // We have work to do, check if left and right values are available, else schedule work
+            let mut new_jobs = vec![job.clone()]; // Reschedule this job last in the stack
 
+            // Ignore unsolved leaf nodes
             if monkey.left == "" || monkey.right == "" {
                 continue;
             }
 
-            let monkey_left = &day.monkeys[&monkey.left];
-            let monkey_right = &day.monkeys[&monkey.right];
-
+            // Only try a node twice, else it is currently unsolveable
             if monkey.try_count > 2 {
                 continue;
             }
 
-            if let (Some(value1), Some(value2)) = (monkey_left.value, monkey_right.value) {
+            // Get the values to operate on
+            let monkey_left = &day.monkeys[&monkey.left];
+            let monkey_right = &day.monkeys[&monkey.right];
+
+            if let (Some(value_left), Some(value_right)) = (monkey_left.value, monkey_right.value) {
                 // We can comput now, so do it!
-                monkey.value = Some(do_math(monkey.operator, value1, value2));
+                monkey.value = Some(do_math(monkey.operator, value_left, value_right));
                 log::debug!(
                     "Doing math {} = {} {:?} {}, {} {:?} {} = {}",
                     monkey.id,
                     monkey_left.id,
                     monkey.operator,
                     monkey_right.id,
-                    value1,
+                    value_left,
                     monkey.operator,
-                    value2,
+                    value_right,
                     monkey.value.unwrap()
                 );
                 day.monkeys.insert(job.id.clone(), monkey);
                 continue;
             }
+
+            // Cannot do math yet, schedule work for unsolved nodes
             if monkey_left.value.is_none() {
                 new_jobs.push(Work {
                     id: monkey_left.id.clone(),
@@ -85,15 +95,19 @@ fn populate(day: &mut Day21, start_id: &str) {
                 });
             }
 
+            // Push the new jobs onto the stack
             jobs.extend(new_jobs);
         }
     }
 }
 
+// Search the hash map, find the monkey that depends on me
+// The solve_for_left means that my_id is on the left side
+// Returns tuple of (key_id, value_id, operator, solve_for_left)
 fn find_monkeys(day: &Day21, my_id: &str) -> (String, String, Operator, bool) {
-    // Search the hash map, find the monkey that depends on me
     for (key, monkey) in day.monkeys.iter() {
         if monkey.left == my_id {
+            // my_id is on the left side
             return (
                 key.to_string(),
                 monkey.right.to_string(),
@@ -101,6 +115,7 @@ fn find_monkeys(day: &Day21, my_id: &str) -> (String, String, Operator, bool) {
                 true,
             );
         } else if monkey.right == my_id {
+            // my_id is on the right side
             return (
                 key.to_string(),
                 monkey.left.to_string(),
@@ -112,6 +127,9 @@ fn find_monkeys(day: &Day21, my_id: &str) -> (String, String, Operator, bool) {
     panic!("Could not find your monkey");
 }
 
+// Walk a tree of math equations, starting at a target leaf node. The math
+// equations need to be solved using simple algebra. If a value is not known
+// yet, then find parents and solve the equation for the parent node.
 fn populate_backwards(day: &mut Day21, start_id: &str) {
     #[derive(Clone, Debug)]
     struct Work {
@@ -123,50 +141,56 @@ fn populate_backwards(day: &mut Day21, start_id: &str) {
     let mut jobs = vec![job];
 
     while jobs.len() > 0 {
+        // Get the job
         let job = jobs.pop().unwrap();
         let mut monkey = day.monkeys[&job.id].clone();
 
+        // Check the value on the job
         if let Some(_value) = monkey.value {
             panic!("Not expected populated value");
         } else {
-            // We have work to do, check if operators are available, else schedule work
-            let mut new_jobs = vec![job.clone()];
+            // Try to compute the value of the job. Or if it cannot be compute
+            // yet, schedule jobs for the parents.
+            let mut new_jobs = vec![job.clone()]; // Reschedule this job last in the stack
 
+            // Find the parent nodes by searchin th hash map
             let monkey_ids = find_monkeys(day, &job.id);
-            let monkey_id1 = monkey_ids.0;
-            let monkey_id2 = monkey_ids.1;
+            let monkey_id_key = monkey_ids.0;
+            let monkey_id_value = monkey_ids.1;
             let operator = monkey_ids.2;
-            let i_am_part_1 = monkey_ids.3;
-            let monkey1 = &day.monkeys[&monkey_id1];
-            let monkey2 = &day.monkeys[&monkey_id2];
+            let solve_for_left = monkey_ids.3; // Bool for if this node's value is on the left side of the operation
+            let monkey_key = &day.monkeys[&monkey_id_key];
+            let monkey_value = &day.monkeys[&monkey_id_value];
 
-            if let (Some(value1), Some(value2)) = (monkey1.value, monkey2.value) {
+            if let (Some(value_key), Some(value_value)) = (monkey_key.value, monkey_value.value) {
                 // We can comput now, so do it!
-                monkey.value = Some(solve_math(operator, value1, value2, i_am_part_1));
+                monkey.value = Some(solve_math(operator, value_key, value_value, solve_for_left));
                 log::debug!(
                     "Doing math {} = {} {:?} {}, {} {:?} {} = {}",
                     monkey.id,
-                    monkey1.id,
+                    monkey_key.id,
                     operator,
-                    monkey2.id,
-                    value1,
+                    monkey_value.id,
+                    value_key,
                     operator,
-                    value2,
+                    value_value,
                     monkey.value.unwrap()
                 );
                 day.monkeys.insert(job.id.clone(), monkey);
                 continue;
             }
 
-            if monkey2.value.is_none() {
+            // We cannot do math yet, find the needed values
+            if monkey_key.value.is_none() {
                 new_jobs.push(Work {
-                    id: monkey2.id.clone(),
+                    id: monkey_key.id.clone(),
                 });
+            } else {
+                panic!("If we cannot do math, it must be because they key is not yet known");
             }
-            if monkey1.value.is_none() {
-                new_jobs.push(Work {
-                    id: monkey1.id.clone(),
-                });
+
+            if monkey_value.value.is_none() {
+                panic!("To solve equations, the other value must always be already known");
             }
 
             log::debug!("Schedule new jobs for {new_jobs:?}");
@@ -183,6 +207,9 @@ impl Puzzle for Day21 {
             monkeys: HashMap::new(),
         };
 
+        // Parse input like
+        // root: pppw + sjmn
+        // dbpl: 5
         for line in input.lines() {
             let mut monkey = Monkey {
                 ..Default::default()
@@ -213,8 +240,8 @@ impl Puzzle for Day21 {
     }
 
     fn solve_part1(&mut self) -> Result<String> {
+        // Simple solve for root
         populate(self, "root");
-        // log::debug!("Monkies {:#?}", self.monkeys);
         let answer = self.monkeys["root"].value.unwrap();
         Ok(answer.to_string())
     }
@@ -227,37 +254,39 @@ impl Puzzle for Day21 {
     }
 
     fn solve_part2(&mut self) -> Result<String> {
-        // Left side has human for my test and real input
-        let root = &self.monkeys["root"];
-        let id_left = root.left.clone();
-        let id_right = root.right.clone();
+        // Instead of solving for root, solve for humn
 
         // Make sure human is none
         let mut human = self.monkeys["humn"].clone();
         human.value = None;
         self.monkeys.insert("humn".to_string(), human);
 
-        // Solve side 2 first
-        log::debug!("Populate side 2, find {}", id_right);
+        // Left side has human for my test and real input
+        // This could be automated but I am lazy and assume we need to solve for left
+        let root = &self.monkeys["root"];
+        let id_left = root.left.clone();
+        let id_right = root.right.clone();
+
+        // Solve side right first
+        log::debug!("Populate side right, find {}", id_right);
         populate(self, &id_right);
 
-        log::debug!("Trying to pre populate side 1, find {}", id_left);
+        // Solve as much of left side as possible
+        log::debug!("Trying to pre populate side left, find {}", id_left);
         populate(self, &id_left);
 
-        // Now we know what monkey on side 1 value should be
-        let mut monkey1 = self.monkeys[&id_left].clone();
+        // Now we know what the monkey on left's value should be
+        let mut monkey_left = self.monkeys[&id_left].clone();
         let value = self.monkeys[&id_right].value.unwrap();
         log::debug!("Found value {value} for root");
-        monkey1.value = Some(value);
-        self.monkeys.insert(id_left.clone(), monkey1);
+        monkey_left.value = Some(value);
+        self.monkeys.insert(id_left.clone(), monkey_left);
 
         // Now use reverse logic to find human value
-        log::debug!("Solve for human using inverse logic");
-        // log::debug!("Monkies {:#?}", self.monkeys);
+        log::debug!("Solve for human using reverse logic");
         populate_backwards(self, "humn");
         let answer = self.monkeys["humn"].value.unwrap();
 
-        // log::debug!("Monkies {:#?}", self.monkeys);
         Ok(answer.to_string())
     }
 
