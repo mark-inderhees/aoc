b546d3c44be506158bf8cd421d051d0f373ef28f
Mark Inderhees <mark.inderhees@gmail.com>
2022-12-25 14:25:52 -0800

    Board 3d comments

--- rust/src/utils/board3d.rs
+++ rust/src/utils/board3d.rs
@@ -2,40 +2,42 @@ use core::fmt::Debug;
 
 use crate::utils::board::*;
 
-pub type BoardId = usize;
-
+/// A 3D board for walking a player around. It's a 3D cube with 6 sides.
+/// The player walks around the sides. Walls are supported.
 pub struct Board3D<T>
 where
     T: Clone + Copy + Debug + PartialEq + std::fmt::Display,
 {
+    /// The 6 boards
     boards: Vec<Board<T>>,
+
+    /// Configs of how the 6 board edges are connected
     configs: Vec<BoardConfig>,
+
+    /// The players on the 3D map. The player is always present on all 6 boards
+    /// but hidden when not on that board.
     players: Vec<Player3D>,
 }
 
+/// A unique id for each of the 6 faces on the cube
+pub type BoardId = usize;
+
+/// The 3D player. Contains which board the player is currently on. And the
+/// current rotation offset of the player to convert from user commands to
+/// board commands.
 struct Player3D {
     board_id: BoardId,
     direction_offset: i32,
 }
 
+/// Each of the 4 board edges connects to another board
 #[derive(Clone)]
 struct BoardConfig {
     connections: Vec<BoardEdgeConnection>,
 }
 
-impl BoardConfig {
-    pub fn new() -> BoardConfig {
-        BoardConfig {
-            connections: vec![
-                BoardEdgeConnection {
-                    ..Default::default()
-                };
-                4
-            ],
-        }
-    }
-}
-
+/// The 4 types of edges with a default of none. Index of none is set high to
+/// break bad code.
 #[derive(Clone, Default, Debug)]
 pub enum Edge {
     Top = 0,
@@ -46,51 +48,11 @@ pub enum Edge {
     None = 100,
 }
 
-#[derive(Clone, Default)]
-pub struct BoardEdge {
-    id: BoardId,
-    edge: Edge,
-}
-
-#[derive(Clone, Default)]
-struct BoardEdgeConnection {
-    board_edge: BoardEdge,
-    inverse: bool,
-}
-
-#[derive(Clone, Default)]
-pub struct EdgeConnection {
-    board_edge1: BoardEdge,
-    board_edge2: BoardEdge,
-    inverse: bool,
-}
-
-impl EdgeConnection {
-    pub fn new(
-        id1: BoardId,
-        edge1: Edge,
-        id2: BoardId,
-        edge2: Edge,
-        inverse: bool,
-    ) -> EdgeConnection {
-        EdgeConnection {
-            board_edge1: BoardEdge {
-                id: id1,
-                edge: edge1,
-            },
-            board_edge2: BoardEdge {
-                id: id2,
-                edge: edge2,
-            },
-            inverse,
-        }
-    }
-}
-
 impl<T> Board3D<T>
 where
     T: Clone + Copy + Debug + PartialEq + std::fmt::Display,
 {
+    /// Create a new 3D board with 6 empty boards
     pub fn new() -> Board3D<T> {
         Board3D {
             boards: vec![
@@ -106,18 +68,23 @@ where
         }
     }
 
+    /// Populate a row on a specific board
     pub fn push_row(&mut self, board_id: BoardId, row: Vec<T>) {
         self.boards[board_id].push_row(row);
     }
 
+    /// Get the width of the cube
     pub fn width(&self) -> i32 {
         self.boards[0].width()
     }
 
+    /// Get the height of the cube
     pub fn height(&self) -> i32 {
         self.boards[0].height()
     }
 
+    /// Add a player to the board. The player is present on all boards but
+    /// hidden when not actually there.
     pub fn add_player(&mut self, board_id: BoardId, point: BoardPoint, id: T) -> PlayerId {
         for (this_board_id, board) in self.boards.iter_mut().enumerate() {
             let player_id = board.add_player(point, id);
@@ -135,34 +102,43 @@ where
         player_id
     }
 
+    /// Add wall type.
     pub fn add_wall(&mut self, wall: T) {
         for board in self.boards.iter_mut() {
             board.add_wall(wall);
         }
     }
 
+    /// Get which board the player is on and the location on that board.
     pub fn get_player_location(&self, player_id: PlayerId) -> (BoardId, BoardPoint) {
         let player = &self.players[player_id];
         let location = self.boards[player.board_id].get_player_location(player_id);
         (player.board_id, location)
     }
 
+    /// Get the current direction of the player given the input of the last client direction command.
+    /// Converts from client commands into actually board direction.
     pub fn get_player_direction(&self, player_id: PlayerId, direction: Direction) -> Direction {
-        //
+        // Lookup the requested direction index
         let directions = vec![
             Direction::Up,
             Direction::Right,
             Direction::Down,
             Direction::Left,
         ];
-        let direction_offset = self.players[player_id].direction_offset;
         let direction_index = directions.iter().position(|&x| x == direction).unwrap();
+
+        // Use offset to get the real index
+        let direction_offset = self.players[player_id].direction_offset;
         let new_index = (direction_index as i32 + direction_offset)
             .rem_euclid(directions.len() as i32) as usize;
+
+        // Return the actual direction
         let real_direction = directions[new_index];
         real_direction
     }
 
+    /// Configure how boards connect. The connection is applied to both boards.
     pub fn set_edge(&mut self, connection: EdgeConnection) {
         self.configs[connection.board_edge1.id].connections
             [connection.board_edge1.edge.clone() as usize] = BoardEdgeConnection {
@@ -176,6 +152,7 @@ where
         };
     }
 
+    /// Helper function when moving between boards. Convert value from one board to another.
     fn get_new_value(&self, value: i32, inverse: bool) -> i32 {
         assert_eq!(self.width(), self.height()); // This logic assumes cube only
         match inverse {
@@ -184,47 +161,64 @@ where
         }
     }
 
+    /// Helper function when moving between boards. If we are moving to a board
+    /// edge that is different than current direction, we need to rotate the
+    /// direction to compensate for this edge connection type.
+    /// Consider this map folded into a cube:
+    /// _01
+    /// _2_
+    /// 34_
+    /// 5__
+    /// Board 1 bottom meets 2 right. When going down on 1, new direction is left on 2.
+    /// new_edge_index=3
+    /// direction_index=0
+    /// So this function returns offset of 3.
+    /// So for Edge enum
+    ///     Top = 0,    <--- would be here, but now +3
+    ///     Right,
+    ///     Bottom,
+    ///     Left,       <--- ends up here like we need it
     fn get_direction_offset(&self, direction: Direction, new_edge: Edge) -> i32 {
+        let new_edge_index = new_edge as i32;
+
+        // Get requested index.
+        // This order is intentionally different than Edge:: as it should be offset by index 2.
+        // As when moving in one direction, we are actually going to the opposite edge type.
+        // That is moving down goes to the top edge of the next map.
         let directions = vec![
-            // This order is intentionally different than Edge:: as it should be offset by index 2
-            Direction::Down,
-            Direction::Left,
-            Direction::Up,
-            Direction::Right,
+            Direction::Down,  // aka Top
+            Direction::Left,  // aka Right
+            Direction::Up,    // aka Bottom
+            Direction::Right, // aka Left
         ];
         let direction_index = directions.iter().position(|&x| x == direction).unwrap() as i32;
-        let new_edge_index = new_edge as i32;
+
+        // Direction and edge type match, no offset needed
         if new_edge_index == direction_index {
             return 0;
-        } else if new_edge_index > direction_index {
+        }
+
+        // Ensure positive offsets only. Will make mod easier.
+        if new_edge_index > direction_index {
             return new_edge_index - direction_index;
         } else {
             return directions.len() as i32 + new_edge_index - direction_index;
         }
     }
 
+    /// Move the player on the 3d board. If they move off one board move the player to the correct new board.
     pub fn step_player(&mut self, player_id: PlayerId, direction: Direction) -> Option<T> {
-        let player3d = &self.players[player_id];
         let (board_id, location) = self.get_player_location(player_id);
 
         // Use requested direction with current board direction offset to find real direction
-        let directions = vec![
-            Direction::Up,
-            Direction::Right,
-            Direction::Down,
-            Direction::Left,
-        ];
-        let direction_index = directions.iter().position(|&x| x == direction).unwrap();
-        let new_index = (direction_index as i32 + player3d.direction_offset)
-            .rem_euclid(directions.len() as i32) as usize;
-        let real_direction = directions[new_index];
+        let real_direction = self.get_player_direction(player_id, direction);
 
         // Test for moving off board condition
         let mut moved_to_new_board = true;
         let mut connection = BoardEdgeConnection {
             ..Default::default()
         };
-        let mut value = 0;
+        let mut value = 0; // The location value to use on the new board
         if real_direction == Direction::Left && location.x == 0 {
             connection = self.configs[board_id].connections[Edge::Left as usize].clone();
             value = location.y;
@@ -241,9 +235,12 @@ where
             moved_to_new_board = false;
         }
 
+        // Need to move boards
         if moved_to_new_board {
             let new_board_id = connection.board_edge.id;
             let new_board_edge = connection.board_edge.edge;
+
+            // Get location on the new board
             let new_location = match new_board_edge {
                 Edge::Left => BoardPoint {
                     x: 0,
@@ -265,24 +262,31 @@ where
             };
 
             if !self.boards[new_board_id].is_wall_here(new_location) {
+                // Move to new board, updating player location and visibility
                 log::debug!("Moving to new board {board_id} -> {new_board_id}");
                 self.boards[new_board_id].set_player_location(player_id, new_location);
                 self.boards[board_id].set_player_visible(player_id, false);
                 self.boards[new_board_id].set_player_visible(player_id, true);
-                self.players[player_id].direction_offset +=
-                    self.get_direction_offset(real_direction, new_board_edge);
                 self.players[player_id].board_id = new_board_id;
 
+                // Update direction compenstation for moving between boards
+                self.players[player_id].direction_offset +=
+                    self.get_direction_offset(real_direction, new_board_edge);
+
+                // Return the value of the grid at this location
                 return Some(self.boards[new_board_id].get_at(new_location));
             } else {
+                // Cannot move to new board, stay on current board
                 log::debug!("Cannot move to new board, there is a wall on {new_board_id} at {new_location:?}");
                 return None;
             }
         }
 
-        return self.boards[board_id].step_player(player_id, real_direction);
+        // Do not need to move boards, do a simple move on current board
+        self.boards[board_id].step_player(player_id, real_direction)
     }
 
+    /// Print each of the 6 boards
     #[allow(dead_code)]
     pub fn print_board3d_with_players_pretty(&mut self) {
         for (i, board) in self.boards.iter_mut().enumerate() {
@@ -291,4 +295,68 @@ where
             println!("");
         }
     }
+}
+
+impl BoardConfig {
+    /// Create a default config, with empty connects for each of the 4 edges
+    pub fn new() -> BoardConfig {
+        BoardConfig {
+            connections: vec![
+                BoardEdgeConnection {
+                    ..Default::default()
+                };
+                4
+            ],
+        }
+    }
+}
+
+/// How two boards connect together on their two edges. Use EdgeConnection::new() to create.
+#[derive(Clone, Default)]
+pub struct EdgeConnection {
+    board_edge1: BoardEdge,
+    board_edge2: BoardEdge,
+    inverse: bool,
+}
+
+impl EdgeConnection {
+    /// Create a connection between two boards. Specify the board ids, edge
+    /// types, and if these boards need inverse connection due to flipped board.
+    pub fn new(
+        id1: BoardId,
+        edge1: Edge,
+        id2: BoardId,
+        edge2: Edge,
+        inverse: bool,
+    ) -> EdgeConnection {
+        EdgeConnection {
+            board_edge1: BoardEdge {
+                id: id1,
+                edge: edge1,
+            },
+            board_edge2: BoardEdge {
+                id: id2,
+                edge: edge2,
+            },
+            inverse,
+        }
+    }
+}
+
+/// Internal use only.
+/// The board edge, used to define board connections. Has the id of this board
+/// and the edge of this board.
+#[derive(Clone, Default)]
+struct BoardEdge {
+    id: BoardId,
+    edge: Edge,
+}
+
+/// Internal use only.
+/// The edge connection info. Has id of board, type of edge, and if the value
+/// needs to be inverted due to flipped board connections.
+#[derive(Clone, Default)]
+struct BoardEdgeConnection {
+    board_edge: BoardEdge,
+    inverse: bool,
 }
