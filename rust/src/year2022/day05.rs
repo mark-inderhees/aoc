2e253a9de3ae0873ef9f21151871acba095ded49
Mark Inderhees <mark.inderhees@gmail.com>
2022-12-25 13:11:44 -0800

    Day 5 header and comments

--- rust/src/year2022/day05.rs
+++ rust/src/year2022/day05.rs
@@ -1,8 +1,14 @@
+// 2022 Day 5
+// https://adventofcode.com/2022/day/5
+// --- Day 5: Supply Stacks ---
+// We have stacks of blocks. Need to move around the stacks.
+// Can either be simple pop and push each block. Or pull full stack and extend.
+
 use std::collections::VecDeque;
 
 use anyhow::Result;
 
-use crate::puzzle::Puzzle;
+use crate::{puzzle::Puzzle, utils::utils::get_vals};
 
 pub struct Day05 {
     stacks: Vec<VecDeque<char>>,
@@ -25,6 +31,8 @@ impl Puzzle for Day05 {
             moves: Vec::new(),
         };
 
+        // Input is really two parts, the game board with current blocks
+        // And the moves to do.
         let inputs: Vec<&str> = input.split("\n\n").collect();
         let board = inputs[0];
         let moves = inputs[1];
@@ -32,6 +40,10 @@ impl Puzzle for Day05 {
         let mut lines: Vec<&str> = board.lines().collect();
         lines.pop(); // Drop index of stacks
         for line in lines {
+            // Line is like
+            //     [N] [C]
+            // [Z] [M] [P]
+            // Start at second char and skip by 4s to get box value
             for (i, c) in line.chars().skip(1).step_by(4).enumerate() {
                 if c != ' ' {
                     day.stacks[i].push_front(c);
@@ -39,15 +51,10 @@ impl Puzzle for Day05 {
             }
         }
 
+        // Moves look like
+        // move 1 from 2 to 1
         for line in moves.lines() {
-            let m: Vec<usize> = line
-                .split(" ")
-                .skip(1)
-                .step_by(2)
-                .collect::<Vec<&str>>()
-                .iter()
-                .map(|a| a.parse::<usize>().unwrap())
-                .collect();
+            let m = get_vals(line);
             day.moves.push(Move {
                 count: m[0],
                 source_index: m[1] - 1,
@@ -59,6 +66,7 @@ impl Puzzle for Day05 {
     }
 
     fn solve_part1(&mut self) -> Result<String> {
+        // For moves, simply pop one item and move it to the destination
         for m in &self.moves {
             for _ in 0..m.count {
                 let c = self.stacks[m.source_index].pop_back().unwrap();
@@ -66,6 +74,7 @@ impl Puzzle for Day05 {
             }
         }
 
+        // Answer is what is on top of each stack
         let mut answer = String::new();
         for stack in &mut self.stacks {
             if stack.len() > 0 {
@@ -84,12 +93,14 @@ impl Puzzle for Day05 {
     }
 
     fn solve_part2(&mut self) -> Result<String> {
+        // For moves, split off from the source and extend destination
         for m in &self.moves {
             let source = &mut self.stacks[m.source_index];
             let c = source.split_off(source.len() - m.count);
             self.stacks[m.dest_index].extend(c);
         }
 
+        // Answer is what is on top of each stack
         let mut answer = String::new();
         for stack in &mut self.stacks {
             if stack.len() > 0 {
