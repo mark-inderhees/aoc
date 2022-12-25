b9218f878c6ea67cf0fba83d1bd5d2cc95168815
Mark Inderhees <mark.inderhees@gmail.com>
2022-12-25 15:09:21 -0800

    Add comments to board struct

--- rust/src/utils/board.rs
+++ rust/src/utils/board.rs
@@ -1,45 +1,40 @@
 use grid::*;
 use rusttype::Point;
-use std::collections::HashMap;
+use std::collections::{HashMap, VecDeque};
 use std::fmt::Debug;
 use std::iter::zip;
 use strum::IntoEnumIterator;
 use strum_macros::EnumIter;
 
-pub type BoardPoint = Point<i32>;
-pub type PlayerId = usize;
-pub const INVALID_PLAYER: usize = usize::MAX;
-
-#[derive(Debug, Clone, Copy)]
-struct Player<T>
-where
-    T: Clone + Copy + Debug + PartialEq + std::fmt::Display,
-{
-    point: BoardPoint,
-    id: T,
-    player_id: PlayerId,
-    visible: bool,
-}
-
-// State about this current square in the gird
-#[derive(Debug, Clone, Copy)]
-struct State {
-    step_count: u32, // Most optimized step count so far at this square
-}
-
+/// A 2d game board for players to navigate.
 pub struct Board<T>
 where
     T: Clone + Copy + Debug + PartialEq + std::fmt::Display,
 {
+    /// The grid itself
     grid: Grid<T>,
     grid_state: Grid<State>,
+
+    /// Players on the grid
     players: Vec<Player<T>>,
+
+    /// Types of walls on the grid
     walls: Vec<T>,
     players_are_walls: bool,
+
+    /// Wraparound supports when reach certain grid value, move to other side
+    /// of row or column
     wraparound: Vec<T>,
     wraparound_mode: bool,
 }
 
+/// Simple way to use Point
+pub type BoardPoint = Point<i32>;
+
+/// Unique id for each player on grid
+pub type PlayerId = usize;
+
+/// Supported directions on the board
 #[derive(Debug, EnumIter, Clone, Copy, PartialEq, Eq, Hash, Default)]
 pub enum Direction {
     #[default]
@@ -55,10 +50,12 @@ pub enum Direction {
 
 impl Direction {
     #[allow(dead_code)]
+    /// Iterate through all directions
     pub fn iterator() -> DirectionIter {
         Direction::iter()
     }
 
+    /// Iterate through just straight directions
     pub fn straight_iterator() -> Vec<Direction> {
         vec![
             Direction::Up,
@@ -68,6 +65,7 @@ impl Direction {
         ]
     }
 
+    /// Given a direction, give the opposite direction
     pub fn opposite_direction(direction: Direction) -> Direction {
         match direction {
             Direction::Up => Direction::Down,
@@ -83,6 +81,7 @@ impl Direction {
 }
 
 impl ToString for Direction {
+    /// Impl ToString trait to pretty print direction dir in small text format
     fn to_string(&self) -> String {
         let dir_to_string = HashMap::from([
             (Direction::Up, "U"),
@@ -102,6 +101,7 @@ impl<T> Board<T>
 where
     T: Clone + Copy + Debug + PartialEq + std::fmt::Display,
 {
+    /// Create a new, empty board.
     pub fn new() -> Board<T> {
         Board {
             grid: grid![],
@@ -114,10 +114,12 @@ where
         }
     }
 
+    /// Return immutable ref to grid itself.
     pub fn grid(&self) -> &Grid<T> {
         &self.grid
     }
 
+    /// Add a new row to the board
     pub fn push_row(&mut self, row: Vec<T>) {
         let len = row.len();
         self.grid.push_row(row);
@@ -132,6 +134,7 @@ where
         self.grid_state.push_row(empty);
     }
 
+    /// Add a row to the top of the board. Useful for things like tetris that grow up.
     pub fn push_front_row(&mut self, row: Vec<T>) {
         let len = row.len();
         self.grid.insert_row(0, row);
@@ -150,6 +153,7 @@ where
         self.grid_state.insert_row(0, empty);
     }
 
+    /// Add in a new player to the board.
     pub fn add_player(&mut self, point: BoardPoint, id: T) -> PlayerId {
         assert!(point.x < self.width() && point.y < self.height());
         self.players.push(Player {
@@ -161,8 +165,9 @@ where
         self.players.len() - 1
     }
 
-    /// Turn a specific board value into players
-    /// Convert the value on the board to a specific background
+    /// Turn a specific board value into players.
+    /// Convert the value on the board to a specific background.
+    /// Useful when input has a whole bunch of preset players that need to start moving.
     pub fn add_players_from_value(&mut self, player_value: T, background_value: T) {
         for y in 0..self.height() {
             for x in 0..self.width() {
@@ -176,31 +181,40 @@ where
         }
     }
 
+    /// Players can be visible. Useful for when players jump on and off board.
     pub fn set_player_visible(&mut self, id: PlayerId, visible: bool) {
         self.players[id].visible = visible;
     }
 
+    /// How many players there are.
     #[allow(dead_code)]
     pub fn get_players_len(&self) -> usize {
         self.players.len()
     }
 
+    /// Add a new wall type.
     pub fn add_wall(&mut self, wall: T) {
         self.walls.push(wall);
     }
 
+    /// Prevent player collisions by making them walls. Else can flow over each other.
     pub fn set_players_as_walls(&mut self) {
         self.players_are_walls = true;
     }
 
+    /// Wrap around mode will move to the other end of the col or row when this
+    /// type is encountered.
     pub fn add_wraparound(&mut self, wraparound: T) {
         self.wraparound.push(wraparound);
     }
 
+    /// Wrap around mode will move to the other end of the col or row when this
+    /// type is encountered.
     pub fn set_wraparound_mode(&mut self) {
         self.wraparound_mode = true;
     }
 
+    /// Is there a player at this location?
     pub fn is_player_here(&self, location: BoardPoint) -> bool {
         for player in &self.players {
             if player.point == location {
@@ -211,24 +225,28 @@ where
         false
     }
 
-    pub fn which_player_is_here(&self, location: BoardPoint) -> PlayerId {
+    /// Return the first player found at this location.
+    pub fn which_player_is_here(&self, location: BoardPoint) -> Option<PlayerId> {
         for player in &self.players {
             if player.point == location {
-                return player.player_id;
+                return Some(player.player_id);
             }
         }
 
-        INVALID_PLAYER
+        None
     }
 
+    /// Width of grid.
     pub fn width(&self) -> i32 {
         self.grid.cols() as i32
     }
 
+    /// Height of grid.
     pub fn height(&self) -> i32 {
         self.grid.rows() as i32
     }
 
+    /// Set the board value here.
     pub fn set_at(&mut self, point: BoardPoint, value: T) {
         let x_: usize = point.x as usize;
         let y_: usize = point.y as usize;
@@ -244,40 +262,45 @@ where
         self.grid[y_][x_]
     }
 
+    /// Get the grid value at this location. Or if a player is here, give the player value.
     pub fn get_at_with_player(&self, point: BoardPoint) -> T {
         let mut value = self.get_at(point);
-        let player_id = self.which_player_is_here(point);
-        if player_id != INVALID_PLAYER {
+        if let Some(player_id) = self.which_player_is_here(point) {
             value = self.players[player_id].id;
         }
         value
     }
 
+    /// Set the location for player 0.
     pub fn set_location(&mut self, point: BoardPoint) {
         let player = 0;
         self.set_player_location(player, point);
     }
 
+    /// Set the location for a player.
     pub fn set_player_location(&mut self, player: PlayerId, point: BoardPoint) {
         self.players[player].point = point;
     }
 
+    /// Get the location of a player.
     pub fn get_player_location(&self, player: PlayerId) -> BoardPoint {
         self.players[player].point
     }
 
+    /// Get the grid value where player 0 is.
     pub fn get_current_value(&self) -> T {
         let player = 0;
         self.get_player_value(player)
     }
 
+    /// Get the grid value where this player is.
     pub fn get_player_value(&self, player: PlayerId) -> T {
         let x: usize = self.players[player].point.x as usize;
         let y: usize = self.players[player].point.y as usize;
         self.grid[y][x]
     }
 
-    // Search all players, finding the smallest y value
+    // Search all players, finding the smallest y value.
     pub fn get_player_minimum_height(&self) -> i32 {
         let mut min_player_y = self.height() - 1;
         for player in &self.players {
@@ -286,21 +309,25 @@ where
         min_player_y
     }
 
+    /// Step player 0 in this direction.
     pub fn step(&mut self, direction: Direction) -> Option<T> {
         let player = 0;
         self.step_player(player, direction)
     }
 
+    /// Step a player in this direction.
     pub fn step_player(&mut self, player: PlayerId, direction: Direction) -> Option<T> {
         self.step_player_optionally(player, direction, true)
     }
 
+    /// Can this player step in this direction? Does not move the player.
     #[allow(dead_code)]
     pub fn can_step_player(&mut self, player: PlayerId, direction: Direction) -> bool {
         self.step_player_optionally(player, direction, false)
             .is_some()
     }
 
+    /// Is there a wall at this location?
     pub fn is_wall_here(&self, point: BoardPoint) -> bool {
         let value = self.get_at(point);
         if self.walls.contains(&value) {
@@ -333,6 +360,9 @@ where
         new_location
     }
 
+    /// Move a player one step in a direction. Or check if it's possible to do the move.
+    /// If possible, returns the grid value at the new location. If requested, player is actually moved.
+    /// If not possible, returns None. Happens when walls are hit or moving off grid.
     fn step_player_optionally(
         &mut self,
         player: PlayerId,
@@ -344,13 +374,15 @@ where
 
         let x_max = self.width();
         let y_max = self.height();
-        if self.wraparound_mode {
-            // Wrap around when moving off grid
-            if new_location.x == -1
-                || new_location.y == -1
-                || new_location.x == x_max
-                || new_location.y == y_max
-            {
+
+        // Check for moving off grid case
+        if new_location.x == -1
+            || new_location.y == -1
+            || new_location.x == x_max
+            || new_location.y == y_max
+        {
+            if self.wraparound_mode {
+                // Wrap around to other side
                 match direction {
                     Direction::Up => new_location.y = y_max - 1,
                     Direction::Down => new_location.y = 0,
@@ -358,29 +390,29 @@ where
                     Direction::Right => new_location.x = 0,
                     _ => panic!("Unsupported wrap around direction"),
                 }
-            }
-        } else {
-            // Fail this move if trying to move off grid
-            match new_location {
-                _ if new_location.x == -1 => return None,
-                _ if new_location.y == -1 => return None,
-                _ if new_location.x == x_max => return None,
-                _ if new_location.y == y_max => return None,
-                _ => (),
+            } else {
+                // Fail this move if trying to move off grid
+                return None;
             }
         }
 
         let mut value = self.get_at(new_location);
+
         if self.wraparound.contains(&value) {
+            // Special wrap around square
             value = self.step_player_wraparound(&mut new_location, direction);
         }
+
+        // Check for walls
         if self.walls.contains(&value) {
             return None;
         }
         if self.players_are_walls && self.is_player_here(new_location) {
             return None;
         }
+
         if do_step {
+            // Actually move the player
             self.players[player] = Player {
                 point: new_location,
                 id: self.players[player].id,
@@ -388,46 +420,38 @@ where
                 visible: self.players[player].visible,
             };
         }
+
         Some(value)
     }
 
-    /// When a wrap around happens, need to find the first non wrap around value
+    /// When a wrap around happens, need to find the first non wrap around
+    /// value on the other side of the row or column.
     fn step_player_wraparound(&self, location: &mut BoardPoint, direction: Direction) -> T {
-        if direction == Direction::Up || direction == Direction::Down {
+        let (offset_x, offset_y, start_x, start_y) = match direction {
             // Search the column
-            let (offset, start_y) = match direction {
-                Direction::Up => (-1, self.height() - 1),
-                Direction::Down => (1, 0),
-                _ => panic!("Unexpected"),
-            };
-            location.y = start_y;
-            loop {
-                let value = self.grid[location.y as usize][location.x as usize];
-                if !self.wraparound.contains(&value) {
-                    return value;
-                }
-                location.y += offset;
-            }
-        } else if direction == Direction::Left || direction == Direction::Right {
+            Direction::Up => (0, -1, location.x, self.height() - 1),
+            Direction::Down => (0, 1, location.x, 0),
+
             // Search the row
-            let (offset, start_x) = match direction {
-                Direction::Left => (-1, self.width() - 1),
-                Direction::Right => (1, 0),
-                _ => panic!("Unexpected"),
-            };
-            location.x = start_x;
-            loop {
-                let value = self.grid[location.y as usize][location.x as usize];
-                if !self.wraparound.contains(&value) {
-                    return value;
-                }
-                location.x += offset;
+            Direction::Left => (-1, 0, self.width() - 1, location.y),
+            Direction::Right => (1, 0, 0, location.y),
+            _ => panic!("Unexpected"),
+        };
+
+        // Search the row or column
+        location.x = start_x;
+        location.y = start_y;
+        loop {
+            let value = self.grid[location.y as usize][location.x as usize];
+            if !self.wraparound.contains(&value) {
+                return value;
             }
-        } else {
-            panic!("Unsupported wrap around direction");
+            location.x += offset_x;
+            location.y += offset_y;
         }
     }
 
+    /// Are these two players near each other? Diagonals are searched.
     pub fn is_nearby(&self, player1: PlayerId, player2: PlayerId) -> bool {
         let p1 = self.players[player1];
         let p2 = self.players[player2];
@@ -441,6 +465,7 @@ where
         false
     }
 
+    /// Is any player nearby this player? Diagonals are searched.
     pub fn is_any_player_nearby(&mut self, player: PlayerId) -> bool {
         for player2 in &self.players {
             if player2.player_id == player {
@@ -454,8 +479,10 @@ where
         false
     }
 
+    /// Get all of the directions this player can move. A list of valid
+    /// directions is returned. Only straight steps are checked.
     #[allow(dead_code)]
-    pub fn get_nearby_squares(&mut self, player: PlayerId) -> Vec<Direction> {
+    pub fn get_directions_player_can_move(&mut self, player: PlayerId) -> Vec<Direction> {
         let mut values = vec![];
         let orig_point = self.get_player_location(player);
         for direction in Direction::straight_iterator() {
@@ -483,6 +510,8 @@ where
         values
     }
 
+    /// Return how a player should move to move towards a destination.
+    /// Diagonals moves are included.
     pub fn where_to_move(&self, start: PlayerId, destination: PlayerId) -> Direction {
         let s = self.players[start];
         let d = self.players[destination];
@@ -501,6 +530,8 @@ where
         }
     }
 
+    /// Return how a player should move to move towards a destination. Only
+    /// straight moves allowed.
     #[allow(dead_code)]
     pub fn where_to_move_straight(&self, start: PlayerId, destination: PlayerId) -> Direction {
         let s = self.players[start];
@@ -519,15 +550,18 @@ where
         }
     }
 
+    /// Print the board with players.
     pub fn print_board_with_players(&mut self) {
         self.print_board_with_players_helper(false);
     }
 
+    /// Print the boards with players using println.
     #[allow(dead_code)]
     pub fn print_board_with_players_pretty(&mut self) {
         self.print_board_with_players_helper(true);
     }
 
+    /// Print the board with players.
     fn print_board_with_players_helper(&mut self, pretty: bool) {
         let orig: Vec<T> = self
             .players
@@ -567,15 +601,19 @@ where
             location: BoardPoint,
             count: u32,
         }
-        let mut jobs = vec![PathWork {
+        let mut jobs = VecDeque::new();
+
+        // Start at the current location
+        jobs.push_front(PathWork {
             location: self.get_player_location(from_player),
             count: 0,
-        }];
-        let mut shortest_path = u32::MAX;
+        });
+
+        let mut shortest_path = u32::MAX; // Best answer so far
         let taget = self.get_player_location(taget_player);
 
         while jobs.len() > 0 {
-            let job = jobs.pop().unwrap();
+            let job = jobs.pop_front().unwrap();
 
             // Check if this count is already too long
             if job.count >= shortest_path {
@@ -614,7 +652,7 @@ where
                         }
 
                         // We can move, so do it!
-                        jobs.push(PathWork {
+                        jobs.push_back(PathWork {
                             location: new_location,
                             count: job.count + 1,
                         });
@@ -626,6 +664,7 @@ where
         shortest_path
     }
 
+    /// Draw a manhattan circle on the board.
     #[allow(dead_code)]
     pub fn draw_manhattan_radius(&mut self, point: BoardPoint, dist: i32, value: T) {
         let mut x_offset = 0;
@@ -645,4 +684,28 @@ where
             x_offset -= 1;
         }
     }
+}
+
+/// Internal only.
+/// A player that is on the board.
+#[derive(Debug, Clone, Copy)]
+struct Player<T>
+where
+    T: Clone + Copy + Debug + PartialEq + std::fmt::Display,
+{
+    point: BoardPoint,
+
+    /// How the player looks when the board is printed
+    id: T,
+
+    player_id: PlayerId,
+    visible: bool,
+}
+
+/// Internal only.
+/// State about this current square in the gird
+#[derive(Debug, Clone, Copy)]
+struct State {
+    /// Most optimized step count so far at this square
+    step_count: u32,
 }
