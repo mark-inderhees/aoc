e6e90d66709422252bcf7b1c0c629e039f6eaac7
Mark Inderhees <mark.inderhees@gmail.com>
2022-12-30 15:24:04 -0800

    Day 4 part 1

--- rust/src/year2015/day04.rs
+++ rust/src/year2015/day04.rs
@@ -3,6 +3,7 @@
 // --- Day 4: The Ideal Stocking Stuffer ---
 
 use anyhow::Result;
+use md5;
 
 use crate::puzzle::Puzzle;
 
@@ -12,25 +13,40 @@ use crate::utils::utils::*;
 #[allow(unused_imports)]
 use std::collections::VecDeque;
 
-pub struct Day04 {}
+pub struct Day04 {
+    input: String,
+}
 
 impl Puzzle for Day04 {
     #[allow(unused_variables)]
     fn from_input(input: &str) -> Result<Self> {
         #[allow(unused_mut)]
-        let mut day = Day04 {};
+        let mut day = Day04 {
+            input: input.lines().collect::<Vec<&str>>()[0].to_string(),
+        };
 
         Ok(day)
     }
 
     fn solve_part1(&mut self) -> Result<String> {
-        Ok("to do".to_string())
+        let mut answer = 0;
+        loop {
+            let md5_input = format!("{}{}", self.input, answer);
+            let digest = md5::compute(md5_input);
+            let hash = format!("{:x}", digest);
+            log::debug!("hash {hash}");
+            if hash.starts_with("00000") {
+                break;
+            }
+            answer +=1;
+        }
+        Ok(answer.to_string())
     }
 
     fn answer_part1(&mut self, test: bool) -> Option<String> {
         match test {
-            true => None,
-            false => None,
+            true => Some(1048970.to_string()),
+            false => Some(254575.to_string()),
         }
     }
 
