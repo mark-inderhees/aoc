827a4210f108738647bbad0364a8fffc5fc933ff
Mark Inderhees <mark.inderhees@gmail.com>
2023-01-01 17:27:56 -0800

    day 7 wip

--- rust/src/year2015/day07.rs
+++ rust/src/year2015/day07.rs
@@ -1,5 +1,6 @@
 // 2015 Day 7
 // https://adventofcode.com/2015/day/7
+// --- Day 7: Some Assembly Required ---
 
 use anyhow::Result;
 
@@ -11,13 +12,49 @@ use crate::utils::utils::*;
 #[allow(unused_imports)]
 use std::collections::VecDeque;
 
-pub struct Day07 {}
+pub struct Day07 {
+    operations: Vec<Operation>,
+}
+
+enum Operators {
+    Set(u16),
+    And,
+    Or,
+    Not,
+    LShift(u16),
+    RShift(u16),
+}
+
+struct Operation {
+    name: String,
+    result: u16,
+    operator: Operators,
+    lhs: String,
+    rhs: String,
+}
 
 impl Puzzle for Day07 {
     #[allow(unused_variables)]
     fn from_input(input: &str) -> Result<Self> {
         #[allow(unused_mut)]
-        let mut day = Day07 {};
+        let mut day = Day07 { operations: vec![] };
+
+        for line in input.lines() {
+            let mut operator =if line.contains("AND") {
+                Operators::And
+            }
+            else if line.contains("OR") {
+                Operators::Or}
+                else if line.contains("Not") {
+                    Operators::Not
+            } else if line.contains("LSHIFT") {
+                Operators::LShift(find_val(line))
+            } else if line.contains("RSHIFT"){
+                Operators::RShift(find_val(line))
+            } else {
+                Operators::Set(find_val(line))
+            };
+        }
 
         Ok(day)
     }
