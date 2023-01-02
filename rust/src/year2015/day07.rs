8610c85b23b170b42b46ac29765f00dc3452a32e
Mark Inderhees <markind@fb.com>
2023-01-01 17:43:17 -0800

    day 7 wip

--- rust/src/year2015/day07.rs
+++ rust/src/year2015/day07.rs
@@ -9,11 +9,12 @@ use crate::puzzle::Puzzle;
 #[allow(unused_imports)]
 use crate::utils::utils::*;
 
+use std::collections::HashMap;
 #[allow(unused_imports)]
 use std::collections::VecDeque;
 
 pub struct Day07 {
-    operations: Vec<Operation>,
+    operations: HashMap<String, Operation>,
 }
 
 enum Operators {
@@ -27,7 +28,7 @@ enum Operators {
 
 struct Operation {
     name: String,
-    result: u16,
+    result: Option<u16>,
     operator: Operators,
     lhs: String,
     rhs: String,
@@ -37,23 +38,62 @@ impl Puzzle for Day07 {
     #[allow(unused_variables)]
     fn from_input(input: &str) -> Result<Self> {
         #[allow(unused_mut)]
-        let mut day = Day07 { operations: vec![] };
+        let mut day = Day07 {
+            operations: HashMap::new(),
+        };
 
         for line in input.lines() {
-            let mut operator =if line.contains("AND") {
+            let operator = if line.contains("AND") {
                 Operators::And
-            }
-            else if line.contains("OR") {
-                Operators::Or}
-                else if line.contains("Not") {
-                    Operators::Not
+            } else if line.contains("OR") {
+                Operators::Or
+            } else if line.contains("Not") {
+                Operators::Not
             } else if line.contains("LSHIFT") {
                 Operators::LShift(find_val(line))
-            } else if line.contains("RSHIFT"){
+            } else if line.contains("RSHIFT") {
                 Operators::RShift(find_val(line))
             } else {
                 Operators::Set(find_val(line))
             };
+
+            let splits: Vec<&str> = line.split(" ").collect();
+            let name = splits.last().unwrap().to_string();
+            let mut lhs = "".to_string();
+            let mut rhs = "".to_string();
+            match operator {
+                Operators::And => {
+                    lhs = splits[0].to_string();
+                    rhs = splits[2].to_string();
+                }
+                Operators::Or => {
+                    lhs = splits[0].to_string();
+                    rhs = splits[2].to_string();
+                }
+                Operators::Not => {
+                    lhs = splits[1].to_string();
+                }
+                Operators::LShift(_) => {
+                    lhs = splits[0].to_string();
+                }
+                Operators::RShift(_) => {
+                    lhs = splits[0].to_string();
+                }
+                Operators::Set(_) => {
+                    // No operands
+                }
+            }
+
+            day.operations.insert(
+                name.clone(),
+                Operation {
+                    name,
+                    result: None,
+                    operator,
+                    lhs,
+                    rhs,
+                },
+            );
         }
 
         Ok(day)
@@ -65,7 +105,7 @@ impl Puzzle for Day07 {
 
     fn answer_part1(&mut self, test: bool) -> Option<String> {
         match test {
-            true => None,
+            true => Some((123 & 456).to_string()),
             false => None,
         }
     }
