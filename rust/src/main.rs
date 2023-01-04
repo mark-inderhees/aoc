179bc157d78ab9fa5a64ced10550e1754b3e6416
Mark Inderhees <markind@fb.com>
2023-01-04 13:17:20 -0800

    Bootstrap 11

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -18,7 +18,7 @@ struct Args {
     #[arg(
         long,
         short,
-        default_value_t = 10, // __BOOTSTRAP_DAY__
+        default_value_t = 11, // __BOOTSTRAP_DAY__
     )]
     day: u32,
 
@@ -240,6 +240,7 @@ fn main() -> Result<()> {
                 8 => run_day::<year2015::day08::Day08>(part, input, test)?,
                 9 => run_day::<year2015::day09::Day09>(part, input, test)?,
                 10 => run_day::<year2015::day10::Day10>(part, input, test)?,
+                11 => run_day::<year2015::day11::Day11>(part, input, test)?,
                 // __BOOTSTRAP_RUN__
                 _ => {
                     println!("Day {} not found, goodbye!\n", day);
---
+++ rust/src/year2015/day11.rs
@@ -0,0 +1,46 @@
+// 2015 Day 11
+// https://adventofcode.com/2015/day/11
+
+use anyhow::Result;
+
+use crate::puzzle::Puzzle;
+
+#[allow(unused_imports)]
+use crate::utils::utils::*;
+
+#[allow(unused_imports)]
+use std::collections::VecDeque;
+
+pub struct Day11 {}
+
+impl Puzzle for Day11 {
+    #[allow(unused_variables)]
+    fn from_input(input: &str) -> Result<Self> {
+        #[allow(unused_mut)]
+        let mut day = Day11 {};
+
+        Ok(day)
+    }
+
+    fn solve_part1(&mut self) -> Result<String> {
+        Ok("to do".to_string())
+    }
+
+    fn answer_part1(&mut self, test: bool) -> Option<String> {
+        match test {
+            true => None,
+            false => None,
+        }
+    }
+
+    fn solve_part2(&mut self) -> Result<String> {
+        Ok("to do".to_string())
+    }
+
+    fn answer_part2(&mut self, test: bool) -> Option<String> {
+        match test {
+            true => None,
+            false => None,
+        }
+    }
+}
