43029bfb72ccbe5c470e7950950063c8fa8b6d83
Mark Inderhees <mark.inderhees@gmail.com>
2023-01-04 22:55:19 -0800

    Day 12 clean

--- rust/src/year2015/day12.rs
+++ rust/src/year2015/day12.rs
@@ -1,21 +1,18 @@
 // 2015 Day 12
 // https://adventofcode.com/2015/day/12
+// Parse json!
 
 use anyhow::Result;
-use json::*;
+use json::*; // This does all the magic!
 
 use crate::puzzle::Puzzle;
-
-#[allow(unused_imports)]
-use crate::utils::utils::*;
-
-#[allow(unused_imports)]
-use std::collections::VecDeque;
 
 pub struct Day12 {
     parsed: JsonValue,
 }
 
+/// Walk all of the array members within a json value. Sum up all numbers.
+/// Optionally ignore any objects that contain a string with "red".
 fn walk_array(arr: &JsonValue, ignore_red: bool) -> f64 {
     let mut output: f64 = 0f64;
     for value in arr.members() {
@@ -27,7 +24,7 @@ fn walk_array(arr: &JsonValue, ignore_red: bool) -> f64 {
             let number: f64 = value.as_number().unwrap().into();
             output += number;
         } else if value.is_string() {
-            // TODO what to do with string
+            // Do nothing with strings inside arrays
         } else {
             panic!("Unexpected value type in array");
         }
@@ -36,6 +33,8 @@ fn walk_array(arr: &JsonValue, ignore_red: bool) -> f64 {
     output
 }
 
+/// Walk all of the object entries within a json value. Sum up all numbers.
+/// Optionally ignore any objects that contain a string with "red".
 fn walk_object(obj: &JsonValue, ignore_red: bool) -> f64 {
     let mut output: f64 = 0f64;
     for (_key, value) in obj.entries() {
@@ -49,6 +48,7 @@ fn walk_object(obj: &JsonValue, ignore_red: bool) -> f64 {
         } else if value.is_string() {
             let string = value.as_str().unwrap();
             if ignore_red && string == "red" {
+                // Ignore objects that contain a string with "red"
                 return 0f64;
             }
         } else {
@@ -67,12 +67,15 @@ impl Puzzle for Day12 {
             parsed: JsonValue::new_array(),
         };
 
+        // The input is pure json. Contains objects, arrays, numbers, and strings.
+        // The root is always an object.
         day.parsed = json::parse(input).unwrap();
 
         Ok(day)
     }
 
     fn solve_part1(&mut self) -> Result<String> {
+        // Sum all numbers present
         let answer = walk_object(&self.parsed, false);
         Ok(answer.to_string())
     }
@@ -85,6 +88,7 @@ impl Puzzle for Day12 {
     }
 
     fn solve_part2(&mut self) -> Result<String> {
+        // Sum all numbers present. Unless the object contains a string with "red".
         let answer = walk_object(&self.parsed, true);
         Ok(answer.to_string())
     }
@@ -92,7 +96,7 @@ impl Puzzle for Day12 {
     fn answer_part2(&mut self, test: bool) -> Option<String> {
         match test {
             true => Some(4.to_string()),
-            false => None,
+            false => Some(65402.to_string()),
         }
     }
 }
