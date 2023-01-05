4f481178de022fb2ffd2aa899b69c792d74961f0
Mark Inderhees <mark.inderhees@gmail.com>
2023-01-04 23:05:58 -0800

    Bootstrap 13

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -18,7 +18,7 @@ struct Args {
     #[arg(
         long,
         short,
-        default_value_t = 12, // __BOOTSTRAP_DAY__
+        default_value_t = 13, // __BOOTSTRAP_DAY__
     )]
     day: u32,
 
@@ -242,6 +242,7 @@ fn main() -> Result<()> {
                 10 => run_day::<year2015::day10::Day10>(part, input, test)?,
                 11 => run_day::<year2015::day11::Day11>(part, input, test)?,
                 12 => run_day::<year2015::day12::Day12>(part, input, test)?,
+                13 => run_day::<year2015::day13::Day13>(part, input, test)?,
                 // __BOOTSTRAP_RUN__
                 _ => {
                     println!("Day {} not found, goodbye!\n", day);
