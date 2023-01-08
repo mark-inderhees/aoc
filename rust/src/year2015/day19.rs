99acf1d697e6d020ef4108ada4211fae5ff61976
Mark Inderhees <markind@fb.com>
2023-01-07 19:15:07 -0800

    Part 2 test works, but probably cannot brute force this

--- rust/src/year2015/day19.rs
+++ rust/src/year2015/day19.rs
@@ -12,7 +12,7 @@ use std::collections::HashMap;
 pub struct Day19 {
     molecule: Molecule,
     replacements: Vec<Replacement>,
-    starts: Vec<String>,
+    starts: Vec<Molecule>,
 }
 
 #[derive(Debug)]
@@ -28,28 +28,59 @@ fn count_replacements(day: &Day19) -> usize {
     for replacement in day.replacements.iter() {
         let molecules = day.molecule.replace(&replacement.from, &replacement.to);
         for molecule in molecules {
-            log::debug!(
-                "Replacement {replacement:?} made new molecule {}",
-                molecule.to_string()
-            );
-            if count.contains_key(&molecule.to_string()) {
-                log::debug!("Molecule already exists");
-            }
             count.insert(molecule.to_string(), true);
             count2 += 1;
         }
     }
 
-    // log::debug!("{:#?}", count);
-    log::info!("Counts {} vs {count2}", count.len());
+    log::debug!("Counts {} vs {count2}", count.len());
 
     count.len()
 }
 
-fn find_best_replacement_path(day: &Day19) -> usize {
-    let mut best = usize::MAX;
+fn find_best_replacement_path(day: &Day19) -> u32 {
+    let mut best = u32::MAX;
+    let max_len = day.molecule.len();
 
-    for start in day.starts.iter() {}
+    struct Work {
+        molecule: Molecule,
+        steps: u32,
+    }
+    let mut jobs = vec![];
+
+    for start in day.starts.iter() {
+        jobs.push(Work {
+            molecule: start.clone(),
+            steps: 1,
+        });
+    }
+
+    while jobs.len() > 0 {
+        let job = jobs.pop().unwrap();
+
+        if job.molecule.len() > max_len {
+            continue;
+        }
+
+        if job.steps >= best {
+            continue;
+        }
+
+        if job.molecule == day.molecule {
+            log::debug!("Built correct molecule after {} steps", job.steps);
+            best = std::cmp::min(best, job.steps);
+        }
+
+        for replacement in day.replacements.iter() {
+            let molecules = job.molecule.replace(&replacement.from, &replacement.to);
+            for molecule in molecules {
+                jobs.push(Work {
+                    molecule,
+                    steps: job.steps + 1,
+                })
+            }
+        }
+    }
 
     best
 }
@@ -69,7 +100,7 @@ impl Puzzle for Day19 {
         for line in split[0].lines() {
             let molecules: Vec<&str> = line.split(" ").collect();
             if line.starts_with("e") {
-                day.starts.push(molecules[2].to_string());
+                day.starts.push(Molecule::new_from_string(molecules[2]));
             } else {
                 day.replacements.push(Replacement {
                     from: Atom::new(molecules[0]),
@@ -78,7 +109,7 @@ impl Puzzle for Day19 {
             }
         }
 
-        log::debug!("{}", day.molecule.to_string());
+        log::debug!("{}, {}", day.molecule.to_string(), day.molecule.len());
         log::trace!("{:#?}", day.replacements);
 
         Ok(day)
