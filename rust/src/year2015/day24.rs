c374d3d968eb30dd16b79e87cf288a55b4c15acf
Mark Inderhees <mark.inderhees@gmail.com>
2023-01-14 13:30:31 -0800

    Part 1 and 2 done, dont overflow 32bit yo

--- rust/src/year2015/day24.rs
+++ rust/src/year2015/day24.rs
@@ -14,11 +14,11 @@ use crate::utils::utils::*;
 use std::collections::VecDeque;
 
 pub struct Day24 {
-    presents: Vec<u32>,
+    presents: Vec<u64>,
 }
 
 /// Remove all items from the original list, returning a new smaller list.
-fn remove_vec_items(original: &Vec<u32>, to_remove: &Vec<u32>) -> Vec<u32> {
+fn remove_vec_items(original: &Vec<u64>, to_remove: &Vec<u64>) -> Vec<u64> {
     let mut output = original.clone();
 
     for item in to_remove.iter() {
@@ -29,9 +29,9 @@ fn remove_vec_items(original: &Vec<u32>, to_remove: &Vec<u32>) -> Vec<u32> {
     output
 }
 
-fn find_lowest_quantum_of_fewest_front_seat_balanced_presents(presents: &Vec<u32>) -> u32 {
+fn find_lowest_quantum_of_fewest_front_seat_balanced_presents(presents: &Vec<u64>) -> u64 {
     // Need to balance presents into 3 equal weight groups
-    let total_weight: u32 = presents.iter().sum();
+    let total_weight: u64 = presents.iter().sum();
     assert_eq!(total_weight % 3, 0);
     let group_weight = total_weight / 3;
     log::debug!("Total weight {total_weight}, looking for group weight {group_weight}");
@@ -39,13 +39,13 @@ fn find_lowest_quantum_of_fewest_front_seat_balanced_presents(presents: &Vec<u32
     let mut balanced_sets = vec![];
 
     let mut smallest_front_len = usize::MAX;
-    let mut smallest_front_quantum = u32::MAX;
+    let mut smallest_front_quantum = u64::MAX;
 
     'loop_len1: for len1 in 1..presents.len() - 2 {
         'loop_group1: for group1 in presents.iter().combinations(len1) {
             log::trace!("Group1 {group1:?}");
-            let group1: Vec<u32> = group1.iter().map(|&&x| x.clone()).collect();
-            let sum1: u32 = group1.iter().sum();
+            let group1: Vec<u64> = group1.iter().map(|&&x| x.clone()).collect();
+            let sum1: u64 = group1.iter().sum();
             if sum1 != group_weight {
                 continue;
             }
@@ -54,7 +54,7 @@ fn find_lowest_quantum_of_fewest_front_seat_balanced_presents(presents: &Vec<u32
                 // There are no more small front groups, all done
                 break 'loop_len1;
             }
-            let quantum: u32 = group1.iter().product();
+            let quantum: u64 = group1.iter().product();
             if quantum > smallest_front_quantum {
                 continue;
             }
@@ -62,13 +62,13 @@ fn find_lowest_quantum_of_fewest_front_seat_balanced_presents(presents: &Vec<u32
             let back_presents = remove_vec_items(presents, &group1);
             for len2 in 1..back_presents.len() - 1 {
                 for group2 in back_presents.iter().combinations(len2) {
-                    let group2: Vec<u32> = group2.iter().map(|&&x| x.clone()).collect();
-                    let sum2: u32 = group2.iter().sum();
+                    let group2: Vec<u64> = group2.iter().map(|&&x| x.clone()).collect();
+                    let sum2: u64 = group2.iter().sum();
                     if sum2 != group_weight {
                         continue;
                     }
                     let group3 = remove_vec_items(&back_presents, &group2);
-                    let sum3: u32 = group3.iter().sum();
+                    let sum3: u64 = group3.iter().sum();
                     if sum3 != group_weight {
                         continue;
                     }
@@ -96,7 +96,7 @@ fn find_lowest_quantum_of_fewest_front_seat_balanced_presents(presents: &Vec<u32
     log::debug!("Groups {balanced_sets:?}");
 
     let front_set_len = balanced_sets[0].0.len();
-    let mut small_front_sets: Vec<&(Vec<u32>, Vec<u32>, Vec<u32>)> = balanced_sets
+    let mut small_front_sets: Vec<&(Vec<u64>, Vec<u64>, Vec<u64>)> = balanced_sets
         .iter()
         .filter(|x| x.0.len() == front_set_len)
         .collect();
@@ -104,10 +104,108 @@ fn find_lowest_quantum_of_fewest_front_seat_balanced_presents(presents: &Vec<u32
     log::debug!("Small front groups {small_front_sets:?}");
 
     // Sort by quantum of first group
-    small_front_sets.sort_by(|a, b| a.0.iter().product::<u32>().cmp(&b.0.iter().product()));
+    small_front_sets.sort_by(|a, b| a.0.iter().product::<u64>().cmp(&b.0.iter().product()));
     log::debug!("Small front groups quantum {small_front_sets:?}");
 
     return small_front_sets[0].0.iter().product();
+}
+
+fn find_lowest_quantum_of_fewest_front_seat_balanced_presents4(presents: &Vec<u64>) -> u64 {
+    // Need to balance presents into 3 equal weight groups
+    let total_weight: u64 = presents.iter().sum();
+    assert_eq!(total_weight % 4, 0);
+    let group_weight = total_weight / 4;
+    log::debug!("Total weight {total_weight}, looking for group weight {group_weight}");
+
+    let mut balanced_sets = vec![];
+
+    let mut smallest_front_len = usize::MAX;
+    let mut smallest_front_quantum = u64::MAX;
+
+    'loop_len1: for len1 in 1..presents.len() - 3 {
+        'loop_group1: for group1 in presents.iter().combinations(len1) {
+            log::trace!("Group1 {group1:?}");
+            let group1: Vec<u64> = group1.iter().map(|&&x| x.clone()).collect();
+            let sum1: u64 = group1.iter().sum();
+            if sum1 != group_weight {
+                log::trace!("Sum1 no good");
+                continue;
+            }
+
+            if len1 > smallest_front_len {
+                // There are no more small front groups, all done
+                log::trace!("All done looping");
+                break 'loop_len1;
+            }
+            let quantum: u64 = group1.iter().product();
+            if quantum > smallest_front_quantum {
+                log::trace!("Too large quantum");
+                continue;
+            }
+
+            let back_presents = remove_vec_items(presents, &group1);
+            for len2 in 1..back_presents.len() - 2 {
+                for group2 in back_presents.iter().combinations(len2) {
+                    let group2: Vec<u64> = group2.iter().map(|&&x| x.clone()).collect();
+                    let sum2: u64 = group2.iter().sum();
+                    if sum2 != group_weight {
+                        log::trace!("Sum2 no good {} {:?}", sum2, group2);
+                        continue;
+                    }
+                    let trunk = remove_vec_items(&back_presents, &group2);
+                    for len3 in 1..trunk.len() - 1 {
+                        for group3 in trunk.iter().combinations(len3) {
+                            let group3: Vec<u64> = group3.iter().map(|&&x| x.clone()).collect();
+                            let sum3: u64 = group3.iter().sum();
+                            if sum3 != group_weight {
+                                log::trace!("Sum3 no good {} {:?}", sum3, group3);
+                                continue;
+                            }
+                            let group4 = remove_vec_items(&trunk, &group3);
+                            let sum4: u64 = group4.iter().sum();
+                            if sum4 != group_weight {
+                                log::trace!("Sum4 no good {} {:?}", sum3, group4);
+                                continue;
+                            }
+                            balanced_sets.push((group1.clone(), group2, group3, group4));
+                            log::debug!(
+                                "Found balanced set {:?}, QE {}",
+                                balanced_sets.last().unwrap(),
+                                quantum
+                            );
+
+                            smallest_front_len = std::cmp::min(smallest_front_len, len1);
+                            smallest_front_quantum = std::cmp::min(smallest_front_quantum, quantum);
+
+                            // Don't need any more group1s as exact make up of group2
+                            // and group3 are not important.
+                            continue 'loop_group1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    // Sort by len of first group
+    // balanced_sets.sort_by(|a, b| a.0.len().cmp(&b.0.len()));
+
+    // log::debug!("Groups {balanced_sets:?}");
+
+    // let front_set_len = balanced_sets[0].0.len();
+    // let mut small_front_sets: Vec<&(Vec<u64>, Vec<u64>, Vec<u64>)> = balanced_sets
+    //     .iter()
+    //     .filter(|x| x.0.len() == front_set_len)
+    //     .collect();
+
+    // log::debug!("Small front groups {small_front_sets:?}");
+
+    // // Sort by quantum of first group
+    // small_front_sets.sort_by(|a, b| a.0.iter().product::<u64>().cmp(&b.0.iter().product()));
+    // log::debug!("Small front groups quantum {small_front_sets:?}");
+
+    // return small_front_sets[0].0.iter().product();
+    smallest_front_quantum
 }
 
 impl Puzzle for Day24 {
@@ -131,18 +229,19 @@ impl Puzzle for Day24 {
     fn answer_part1(&mut self, test: bool) -> Option<String> {
         match test {
             true => Some(99.to_string()),
-            false => None,
+            false => Some(11846773891u64.to_string()),
         }
     }
 
     fn solve_part2(&mut self) -> Result<String> {
-        Ok("to do".to_string())
+        let answer = find_lowest_quantum_of_fewest_front_seat_balanced_presents4(&self.presents);
+        Ok(answer.to_string())
     }
 
     fn answer_part2(&mut self, test: bool) -> Option<String> {
         match test {
-            true => None,
-            false => None,
+            true => Some(44.to_string()),
+            false => Some(80393059.to_string()),
         }
     }
 }
