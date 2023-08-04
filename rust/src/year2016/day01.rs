a1a9f457048d463e641a0a1bbbec737759af2ecd
Mark Inderhees <mark.inderhees@gmail.com>
2023-08-04 09:32:34 -0700

    day 1 cleanup

--- rust/src/year2016/day01.rs
+++ rust/src/year2016/day01.rs
@@ -1,5 +1,7 @@
 // 2016 Day 1
 // https://adventofcode.com/2016/day/1
+// --- Day 1: No Time for a Taxicab ---
+// Taxicab geometry around Easter Bunny HQ
 
 use anyhow::Result;
 use rusttype::Point;
@@ -17,7 +19,7 @@ use std::collections::VecDeque;
 
 pub struct Day01 {
     grid: Board<char>,
-    part2: Vec<Point<i32>>,
+    revisit: Vec<Point<i32>>, // Grid locations that have been revisited, in order
 }
 
 impl Puzzle for Day01 {
@@ -26,13 +28,16 @@ impl Puzzle for Day01 {
         #[allow(unused_mut)]
         let mut day = Day01 {
             grid: Board::new(),
-            part2: vec![],
+            revisit: vec![],
         };
 
+        // Create a board that is big enough
         let width = 1000i32;
         for _ in 0..width {
             day.grid.push_row(vec!['0'; width as usize]);
         }
+
+        // Add a player and start point in the middle
         let p1 = day.grid.add_player(
             BoardPoint {
                 x: width / 2,
@@ -48,6 +53,7 @@ impl Puzzle for Day01 {
             's',
         );
 
+        // Can move in 4 directions, commands are rotations
         let mut direction_index = 0i32;
         let directions = vec![
             Direction::Up,
@@ -55,8 +61,12 @@ impl Puzzle for Day01 {
             Direction::Down,
             Direction::Left,
         ];
+
+        // Input looks like R5, which means turn right and move 5 steps
         for command in input.split(",") {
             let command2 = command.trim();
+
+            // For a rotation command, move to next direction
             match command2.chars().next().unwrap() {
                 'R' => direction_index += 1,
                 'L' => direction_index -= 1,
@@ -64,16 +74,20 @@ impl Puzzle for Day01 {
             }
             let direction = directions[direction_index.rem_euclid(4) as usize];
             log::debug!("Direction is {:?} for command {}", direction, command2);
-            let mut bob = command2.chars();
-            bob.next();
-            let steps = find_val(bob.as_str());
+
+            // Now move the number of steps
+            let mut steps_str = command2.chars();
+            steps_str.next();
+            let steps = find_val(steps_str.as_str());
             for _ in 0..steps {
                 day.grid.step_player(p1, direction);
+
+                // Check if we have been at this location already
                 let loc = day.grid.player_location(p1);
                 let value = day.grid.value_at(loc);
                 if value != '0' {
-                    log::debug!("Done at {:?}", loc);
-                    day.part2.push(loc)
+                    log::debug!("Part 2 done at {:?}", loc);
+                    day.revisit.push(loc)
                 }
                 day.grid.set_at(loc, '1');
             }
@@ -82,13 +96,14 @@ impl Puzzle for Day01 {
         Ok(day)
     }
 
+    // Find the taxicab distanced between start and end point
     fn solve_part1(&mut self) -> Result<String> {
-        let point1 = self.grid.player_location(0);
-        let start1 = self.grid.player_location(1);
-        let x = point1.x - start1.x;
-        let y = point1.y - start1.y;
+        let end = self.grid.player_location(0);
+        let start = self.grid.player_location(1);
+        let diff = end - start;
+        let answer = diff.x.abs() + diff.y.abs();
 
-        Ok((x.abs() + y.abs()).to_string())
+        Ok(answer.to_string())
     }
 
     fn answer_part1(&mut self, test: bool) -> Option<String> {
@@ -98,12 +113,12 @@ impl Puzzle for Day01 {
         }
     }
 
+    // Find the taxicab distance between start and first spot revisited
     fn solve_part2(&mut self) -> Result<String> {
-        let start1 = self.grid.player_location(1);
-        let m = self.part2[0];
-        let x = m.x - start1.x;
-        let y = m.y - start1.y;
-        let answer = x.abs() + y.abs();
+        let start = self.grid.player_location(1);
+        let revisit = self.revisit[0];
+        let diff = revisit - start;
+        let answer = diff.x.abs() + diff.y.abs();
         Ok(answer.to_string())
     }
 
