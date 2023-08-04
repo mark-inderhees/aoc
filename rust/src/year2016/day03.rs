3f62674c76a162d8cae539d463828484cef5ed59
Mark Inderhees <mark.inderhees@gmail.com>
2023-08-04 11:01:31 -0700

    bootstrap day3

---
+++ rust/src/year2016/day03.rs
@@ -0,0 +1,46 @@
+// 2016 Day 3
+// https://adventofcode.com/2016/day/3
+
+use anyhow::Result;
+
+use crate::puzzle::Puzzle;
+
+#[allow(unused_imports)]
+use crate::utils::utils::*;
+
+#[allow(unused_imports)]
+use std::collections::VecDeque;
+
+pub struct Day03 {}
+
+impl Puzzle for Day03 {
+    #[allow(unused_variables)]
+    fn from_input(input: &str) -> Result<Self> {
+        #[allow(unused_mut)]
+        let mut day = Day03 {};
+
+        Ok(day)
+    }
+
+    fn solve_part1(&mut self) -> Result<String> {
+        Ok("to do".to_string())
+    }
+
+    fn answer_part1(&mut self, test: bool) -> Option<String> {
+        match test {
+            true => None,
+            false => None,
+        }
+    }
+
+    fn solve_part2(&mut self) -> Result<String> {
+        Ok("to do".to_string())
+    }
+
+    fn answer_part2(&mut self, test: bool) -> Option<String> {
+        match test {
+            true => None,
+            false => None,
+        }
+    }
+}
