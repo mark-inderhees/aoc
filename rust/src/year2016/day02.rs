ed32a1b75055fb5318eabe557a439b713104f5ef
Mark Inderhees <mark.inderhees@gmail.com>
2023-08-04 10:58:27 -0700

    day2 part2

--- rust/src/year2016/day02.rs
+++ rust/src/year2016/day02.rs
@@ -13,27 +13,17 @@ use crate::utils::utils::*;
 use std::collections::VecDeque;
 
 pub struct Day02 {
-    grid: Board<u32>,
-    values: Vec<String>,
+    commands: Vec<Vec<Direction>>,
 }
 
 impl Puzzle for Day02 {
     #[allow(unused_variables)]
     fn from_input(input: &str) -> Result<Self> {
         #[allow(unused_mut)]
-        let mut day = Day02 {
-            grid: Board::new(),
-            values: vec![],
-        };
-
-        day.grid.push_row(vec![1, 2, 3]);
-        day.grid.push_row(vec![4, 5, 6]);
-        day.grid.push_row(vec![7, 8, 9]);
-        let middle = BoardPoint { x: 1, y: 1 };
-        let p1 = day.grid.add_player(middle, 0);
+        let mut day = Day02 { commands: vec![] };
 
         for line in input.trim().split('\n') {
-            // day.grid.set_player_location(p1, middle);
+            let mut command = vec![];
             for direction_char in line.chars() {
                 let direction = match direction_char {
                     'U' => Direction::Up,
@@ -42,18 +32,32 @@ impl Puzzle for Day02 {
                     'R' => Direction::Right,
                     _ => panic!("Unknown direction char"),
                 };
-                day.grid.step_player(p1, direction);
+                command.push(direction)
             }
-            let value = day.grid.player_value(p1);
-            log::debug!("Value is {}", value);
-            day.values.push(value.to_string());
+            day.commands.push(command);
         }
 
         Ok(day)
     }
 
     fn solve_part1(&mut self) -> Result<String> {
-        Ok(self.values.join(""))
+        let mut grid = Board::new();
+        let mut values = vec![];
+        grid.push_row(vec!['1', '2', '3']);
+        grid.push_row(vec!['4', '5', '6']);
+        grid.push_row(vec!['7', '8', '9']);
+        let middle = BoardPoint { x: 1, y: 1 };
+        let p1 = grid.add_player(middle, 'x');
+
+        for command in self.commands.iter() {
+            for direction in command {
+                grid.step_player(p1, *direction);
+            }
+            let value = grid.player_value(p1);
+            log::debug!("Value is {}", value);
+            values.push(value.to_string());
+        }
+        Ok(values.join(""))
     }
 
     fn answer_part1(&mut self, test: bool) -> Option<String> {
@@ -64,12 +68,31 @@ impl Puzzle for Day02 {
     }
 
     fn solve_part2(&mut self) -> Result<String> {
-        Ok("to do".to_string())
+        let mut grid = Board::new();
+        let mut values = vec![];
+        grid.push_row(vec![' ', ' ', '1', ' ', ' ']);
+        grid.push_row(vec![' ', '2', '3', '4', ' ']);
+        grid.push_row(vec!['5', '6', '7', '8', '9']);
+        grid.push_row(vec![' ', 'A', 'B', 'C', ' ']);
+        grid.push_row(vec![' ', ' ', 'D', ' ', ' ']);
+        grid.add_wall(' ');
+        let middle = BoardPoint { x: 0, y: 2 };
+        let p1 = grid.add_player(middle, 'x');
+
+        for command in self.commands.iter() {
+            for direction in command {
+                grid.step_player(p1, *direction);
+            }
+            let value = grid.player_value(p1);
+            log::debug!("Value is {}", value);
+            values.push(value.to_string());
+        }
+        Ok(values.join(""))
     }
 
     fn answer_part2(&mut self, test: bool) -> Option<String> {
         match test {
-            true => None,
+            true => Some("5DB3".to_string()),
             false => None,
         }
     }
