f6ab255a975f659d2dfbe9d33bd5f146d5cca7a1
Mark Inderhees <mark.inderhees@gmail.com>
2023-08-05 18:10:30 -0700

    day 3 part 1

--- rust/src/year2016/day03.rs
+++ rust/src/year2016/day03.rs
@@ -11,25 +11,48 @@ use crate::utils::utils::*;
 #[allow(unused_imports)]
 use std::collections::VecDeque;
 
-pub struct Day03 {}
+#[derive(Debug)]
+struct Triangle {
+    sides: Vec<u32>,
+    valid: bool,
+}
+
+pub struct Day03 {
+    triangles: Vec<Triangle>,
+}
 
 impl Puzzle for Day03 {
     #[allow(unused_variables)]
     fn from_input(input: &str) -> Result<Self> {
         #[allow(unused_mut)]
-        let mut day = Day03 {};
+        let mut day = Day03 { triangles: vec![] };
+
+        for line in input.trim().split("\n") {
+            let mut vals = find_vals(line);
+            vals.sort();
+            let triangle = Triangle {
+                valid: vals[0] + vals[1] > vals[2],
+                sides: vals,
+            };
+            day.triangles.push(triangle);
+        }
+        log::debug!("Triangle {:?}", day.triangles);
 
         Ok(day)
     }
 
     fn solve_part1(&mut self) -> Result<String> {
-        Ok("to do".to_string())
+        let count = self.triangles.iter().fold(0u32, |acc, x| match x.valid {
+            true => 1 + acc,
+            false => acc,
+        });
+        Ok(count.to_string())
     }
 
     fn answer_part1(&mut self, test: bool) -> Option<String> {
         match test {
-            true => None,
-            false => None,
+            true => Some(1.to_string()),
+            false => Some(869.to_string()),
         }
     }
 
