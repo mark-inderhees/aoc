ccedd4e1930fd01f901f50a28054d61749b1e747
Mark Inderhees <mark.inderhees@gmail.com>
2023-08-05 20:09:29 -0700

    day3 clean

--- rust/src/year2016/day03.rs
+++ rust/src/year2016/day03.rs
@@ -1,5 +1,7 @@
 // 2016 Day 3
 // https://adventofcode.com/2016/day/3
+// --- Day 3: Squares With Three Sides ---
+// Determine if lenghts of triangle reveal valid triangles
 
 use anyhow::Result;
 
@@ -16,6 +18,19 @@ pub struct Day03 {
     triangles: Vec<Vec<u32>>,
 }
 
+// Given a triangle with three sides, determine if it is a valid triangle.
+// Return the number of valid triangles.
+fn count_valid_triangles(triangles: &Vec<Vec<u32>>) -> u32 {
+    triangles.iter().fold(0u32, |acc, x| {
+        let mut sides = x.clone();
+        sides.sort();
+        match sides[0] + sides[1] > sides[2] {
+            true => 1 + acc,
+            false => acc,
+        }
+    })
+}
+
 impl Puzzle for Day03 {
     #[allow(unused_variables)]
     fn from_input(input: &str) -> Result<Self> {
@@ -23,6 +38,7 @@ impl Puzzle for Day03 {
         let mut day = Day03 { triangles: vec![] };
 
         for line in input.trim().split("\n") {
+            // Each line has three lengths of the triangle
             let vals = find_vals(line);
             day.triangles.push(vals);
         }
@@ -32,14 +48,8 @@ impl Puzzle for Day03 {
     }
 
     fn solve_part1(&mut self) -> Result<String> {
-        let count = self.triangles.iter().fold(0u32, |acc, x| {
-            let mut sides = x.clone();
-            sides.sort();
-            match sides[0] + sides[1] > sides[2] {
-                true => 1 + acc,
-                false => acc,
-            }
-        });
+        // How many triangles are valid?
+        let count = count_valid_triangles(&self.triangles);
         Ok(count.to_string())
     }
 
@@ -51,6 +61,8 @@ impl Puzzle for Day03 {
     }
 
     fn solve_part2(&mut self) -> Result<String> {
+        // Triangles are actually listed in columns, so read in chuncks of 3
+        // and build real triangles.
         let mut triangles = vec![];
         let chunks: Vec<&[Vec<u32>]> = self.triangles.chunks(3).collect();
         for chunk in chunks {
@@ -58,15 +70,9 @@ impl Puzzle for Day03 {
                 triangles.push(vec![chunk[0][i], chunk[1][i], chunk[2][i]])
             }
         }
-        let count = triangles.iter().fold(0u32, |acc, x| {
-            let mut sides = x.clone();
-            sides.sort();
-            match sides[0] + sides[1] > sides[2] {
-                true => 1 + acc,
-                false => acc,
-            }
-        });
 
+        // Now how many of these real triangles are valid?
+        let count = count_valid_triangles(&triangles);
         Ok(count.to_string())
     }
 
