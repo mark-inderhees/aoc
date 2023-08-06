524778dd0984d434988343a67ba9bcd73076cfaa
Mark Inderhees <mark.inderhees@gmail.com>
2023-08-05 20:04:25 -0700

    day3 part2 done

--- rust/src/year2016/day03.rs
+++ rust/src/year2016/day03.rs
@@ -10,15 +10,10 @@ use crate::utils::utils::*;
 
 #[allow(unused_imports)]
 use std::collections::VecDeque;
-
-#[derive(Debug)]
-struct Triangle {
-    sides: Vec<u32>,
-    valid: bool,
-}
+use std::vec;
 
 pub struct Day03 {
-    triangles: Vec<Triangle>,
+    triangles: Vec<Vec<u32>>,
 }
 
 impl Puzzle for Day03 {
@@ -28,13 +23,8 @@ impl Puzzle for Day03 {
         let mut day = Day03 { triangles: vec![] };
 
         for line in input.trim().split("\n") {
-            let mut vals = find_vals(line);
-            vals.sort();
-            let triangle = Triangle {
-                valid: vals[0] + vals[1] > vals[2],
-                sides: vals,
-            };
-            day.triangles.push(triangle);
+            let vals = find_vals(line);
+            day.triangles.push(vals);
         }
         log::debug!("Triangle {:?}", day.triangles);
 
@@ -42,28 +32,48 @@ impl Puzzle for Day03 {
     }
 
     fn solve_part1(&mut self) -> Result<String> {
-        let count = self.triangles.iter().fold(0u32, |acc, x| match x.valid {
-            true => 1 + acc,
-            false => acc,
+        let count = self.triangles.iter().fold(0u32, |acc, x| {
+            let mut sides = x.clone();
+            sides.sort();
+            match sides[0] + sides[1] > sides[2] {
+                true => 1 + acc,
+                false => acc,
+            }
         });
         Ok(count.to_string())
     }
 
     fn answer_part1(&mut self, test: bool) -> Option<String> {
         match test {
-            true => Some(1.to_string()),
+            true => Some(3.to_string()),
             false => Some(869.to_string()),
         }
     }
 
     fn solve_part2(&mut self) -> Result<String> {
-        Ok("to do".to_string())
+        let mut triangles = vec![];
+        let chunks: Vec<&[Vec<u32>]> = self.triangles.chunks(3).collect();
+        for chunk in chunks {
+            for i in 0..3 {
+                triangles.push(vec![chunk[0][i], chunk[1][i], chunk[2][i]])
+            }
+        }
+        let count = triangles.iter().fold(0u32, |acc, x| {
+            let mut sides = x.clone();
+            sides.sort();
+            match sides[0] + sides[1] > sides[2] {
+                true => 1 + acc,
+                false => acc,
+            }
+        });
+
+        Ok(count.to_string())
     }
 
     fn answer_part2(&mut self, test: bool) -> Option<String> {
         match test {
-            true => None,
-            false => None,
+            true => Some(6.to_string()),
+            false => Some(1544.to_string()),
         }
     }
 }
