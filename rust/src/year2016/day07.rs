3a89a46b0b467a7857baed87bf1dd560ce08c2cd
Mark Inderhees <mark.inderhees@gmail.com>
2023-08-07 09:12:52 -0700

    day 7 clean up

--- rust/src/year2016/day07.rs
+++ rust/src/year2016/day07.rs
@@ -1,5 +1,7 @@
 // 2016 Day 7
 // https://adventofcode.com/2016/day/7
+// --- Day 7: Internet Protocol Version 7 ---
+// Parse strings for repeating patterns
 
 use anyhow::Result;
 
@@ -12,7 +14,7 @@ use crate::utils::utils::*;
 use std::collections::VecDeque;
 
 pub struct Day07 {
-    ips: Vec<String>,
+    ips: Vec<String>, // Input is a list of IPv7s
 }
 
 impl Puzzle for Day07 {
@@ -29,6 +31,7 @@ impl Puzzle for Day07 {
     }
 
     fn solve_part1(&mut self) -> Result<String> {
+        // Find IPs that have abba type of pattern outside of []
         let mut valid_count = 0;
         for ip in &self.ips {
             let mut valid = false;
@@ -37,6 +40,7 @@ impl Puzzle for Day07 {
             log::debug!("IP {}", ip);
             for window in chars.windows(4) {
                 log::debug!("Window {:?}", window);
+                // First check if in or out of brackets
                 if window.contains(&'[') {
                     log::debug!("Start brackets");
                     in_brackets = true;
@@ -44,6 +48,7 @@ impl Puzzle for Day07 {
                     log::debug!("End brackets");
                     in_brackets = false;
                 } else {
+                    // Now test for abba pattern
                     log::debug!(
                         "Testing chars {} {} {} {}",
                         window[0],
@@ -55,6 +60,9 @@ impl Puzzle for Day07 {
                         && (window[1] == window[2])
                         && (window[0] != window[1])
                     {
+                        // Found a match, if in brackets then we known we are
+                        // done and not valid. If out of brackets, this could
+                        // be valid but need to test full IP.
                         if in_brackets {
                             log::debug!("Inside brackets, not valid IP");
                             valid = false;
@@ -82,16 +90,23 @@ impl Puzzle for Day07 {
     }
 
     fn solve_part2(&mut self) -> Result<String> {
+        // Test IP for repeating pattern like that has aba outside of brackets
+        // and bab inside of brackets.
         let mut valid_count = 0;
         for ip in &self.ips {
             let mut valid = false;
             let mut in_brackets = false;
+
+            // Need to keep list of found aba type of patterns both inside and
+            // outside of brackets so only a single pass of IP is needed
             let mut matches_inside = vec![];
             let mut matches_outside = vec![];
+
             let chars: Vec<char> = ip.chars().collect();
             log::debug!("IP {}", ip);
             for window in chars.windows(3) {
                 log::debug!("Window {:?}", window);
+                // First check if inside or out of brackets
                 if window.contains(&'[') {
                     log::debug!("Start brackets");
                     in_brackets = true;
@@ -99,8 +114,11 @@ impl Puzzle for Day07 {
                     log::debug!("End brackets");
                     in_brackets = false;
                 } else {
+                    // Now test for aba type of pattern
                     log::debug!("Testing chars {} {} {}", window[0], window[1], window[2]);
                     if (window[0] == window[2]) && (window[0] != window[1]) {
+                        // Found pattern, build opposite that needs match, then
+                        // look to see if we've found the opposite yet.
                         let opposite: &[char] = &[window[1], window[0], window[1]];
                         if in_brackets {
                             matches_inside.push(window);
