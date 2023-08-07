25026021b34fd30f0d166e47f1198ebed20136db
Mark Inderhees <mark.inderhees@gmail.com>
2023-08-06 20:08:08 -0700

    day 6 part1 done

--- rust/src/year2016/day06.rs
+++ rust/src/year2016/day06.rs
@@ -11,25 +11,50 @@ use crate::utils::utils::*;
 #[allow(unused_imports)]
 use std::collections::VecDeque;
 
-pub struct Day06 {}
+pub struct Day06 {
+    messages: Vec<String>,
+}
 
 impl Puzzle for Day06 {
     #[allow(unused_variables)]
     fn from_input(input: &str) -> Result<Self> {
         #[allow(unused_mut)]
-        let mut day = Day06 {};
+        let mut day = Day06 { messages: vec![] };
+
+        for line in input.trim().split("\n") {
+            day.messages.push(line.to_string());
+        }
 
         Ok(day)
     }
 
     fn solve_part1(&mut self) -> Result<String> {
-        Ok("to do".to_string())
+        let mut answer = String::new();
+        for i in 0..self.messages[0].len() {
+            let mut letter_count = vec![0; 26];
+            for message in &self.messages {
+                let index = message.chars().nth(i).unwrap() as usize - 'a' as usize;
+                letter_count[index] += 1;
+            }
+            let mut max_count = 0;
+            let mut max_index = 0;
+            for (index, count) in letter_count.iter().enumerate() {
+                if *count > max_count {
+                    max_count = *count;
+                    max_index = index;
+                }
+            }
+            let char = char::from_u32(max_index as u32 + 'a' as u32).unwrap();
+            answer += &char.to_string();
+        }
+
+        Ok(answer)
     }
 
     fn answer_part1(&mut self, test: bool) -> Option<String> {
         match test {
-            true => None,
-            false => None,
+            true => Some("easter".to_string()),
+            false => Some("cyxeoccr".to_string()),
         }
     }
 
