b7a100d53dda8cfeb3925101af516351c1d8c473
Mark Inderhees <mark.inderhees@gmail.com>
2023-08-08 22:54:29 -0700

    day9 clean

--- rust/src/year2016/day09.rs
+++ rust/src/year2016/day09.rs
@@ -1,5 +1,7 @@
 // 2016 Day 9
 // https://adventofcode.com/2016/day/9
+// --- Day 9: Explosives in Cyberspace ---
+// Decompress data using a silly format
 
 use anyhow::Result;
 use regex::Regex;
@@ -32,14 +34,19 @@ impl Puzzle for Day09 {
     }
 
     fn solve_part1(&mut self) -> Result<String> {
+        // Find length of decompressed string, compression markers look like
+        // (LxC) for length of chars to read after marker and count for number
+        // of times to repeat that pattern.
+        // If a marker is found inside a marker, then ignore it.
+
         let mut answer = 0;
         enum State {
-            Normal,
-            InMarker,
-            Decompressing,
+            Normal,        // Just copy to decompressed
+            InMarker,      // This is marker
+            Decompressing, // Building a string to repeat
         }
         for compression in self.compressions.iter() {
-            let mut decoded = "".to_string();
+            let mut decoded = "".to_string(); // Final output string
             let mut state = State::Normal;
             let mut marker = "".to_string();
             let mut repeat_str = "".to_string();
@@ -50,6 +57,7 @@ impl Puzzle for Day09 {
                 match state {
                     State::InMarker => match character {
                         ')' => {
+                            // Done reading in a marker, grep out the data and move to decompressing
                             let re = Regex::new(r"(\d*)x(\d*)").unwrap();
                             let matches = re.captures(&marker).unwrap();
                             repeat_len = find_val(matches.get(1).unwrap().as_str());
@@ -58,20 +66,28 @@ impl Puzzle for Day09 {
                             state = State::Decompressing
                         }
                         _ => {
+                            // Add character to marker string
                             marker.push(character);
                         }
                     },
                     State::Normal => match character {
                         '(' => {
+                            // Just found a marker, start new string for it
                             marker = "".to_string();
                             state = State::InMarker;
                         }
-                        _ => decoded += &character.to_string(),
+                        _ => {
+                            // Copy directly to output
+                            decoded += &character.to_string()
+                        }
                     },
                     State::Decompressing => {
+                        // Add to string to be repeated
                         repeat_str.push(character);
                         repeat_index += 1;
                         if repeat_index == repeat_len {
+                            // Done building the repeat string, now add it to
+                            // output and move back to normal state.
                             log::debug!("Found repeat {}", repeat_str);
                             for _ in 0..repeat_count {
                                 decoded += &repeat_str;
@@ -98,85 +114,25 @@ impl Puzzle for Day09 {
     }
 
     fn solve_part2(&mut self) -> Result<String> {
+        // Like part1, but now if a marker is found inside a marker, it applies
+        // at scale. Find the length of the decompressed string.
+
         let mut answer = 0;
-        // enum State {
-        //     Normal,
-        //     InMarker,
-        //     JustFinishedMarker,
-        // }
-        // struct Repeat {
-        //     length: usize,
-        //     count: usize,
-        // }
-
-        // for compression in self.compressions.iter() {
-        //     log::debug!("Decompress {}", compression);
-        //     let mut length = 0;
-        //     let mut state = State::Normal;
-        //     let mut marker = "".to_string();
-        //     let mut repeats: Vec<Repeat> = vec![];
-
-        //     let mut i = 0;
-        //     while i < compression.len() {
-        //         let character = compression.chars().nth(i).unwrap();
-        //         match character {
-        //             '(' => {
-        //                 marker = "".to_string();
-        //                 state = State::InMarker;
-        //             }
-        //             ')' => {
-        //                 let re = Regex::new(r"(\d*)x(\d*)").unwrap();
-        //                 let matches = re.captures(&marker).unwrap();
-        //                 let repeat = Repeat {
-        //                     length: find_val(matches.get(1).unwrap().as_str()),
-        //                     count: find_val(matches.get(2).unwrap().as_str()),
-        //                 };
-        //                 log::debug!(
-        //                     "Found marker {}: {}x{}",
-        //                     marker,
-        //                     repeat.length,
-        //                     repeat.count
-        //                 );
-        //                 repeats.push(repeat);
-        //                 state = State::JustFinishedMarker
-        //             }
-        //             _ => match state {
-        //                 State::InMarker => marker.push(character),
-        //                 State::JustFinishedMarker => {
-        //                     let first_length = repeats.last().unwrap().length;
-        //                     let mut stack_length = first_length;
-        //                     for repeat in repeats.iter() {
-        //                         stack_length *= repeat.count;
-        //                     }
-        //                     i += first_length - 1;
-        //                     length += stack_length;
-        //                     log::debug!("Popped all markers, added length {}", stack_length);
-        //                     state = State::Normal;
-        //                 }
-        //                 State::Normal => {
-        //                     length += 1;
-        //                 }
-        //             },
-        //         }
-        //         i += 1;
-        //     }
-        //     log::debug!("Len {}", length);
-        //     answer = length
-        // }
 
         enum State {
-            Normal,
-            InMarker,
+            Normal,   // Not inside a marker
+            InMarker, // Inside a marker
         }
 
+        // Repeat struct stores info on the marker
         #[derive(Debug)]
         struct Repeat {
-            length: usize,
-            count: usize,
-            index: usize,
-            end_index: usize,
-            characters: usize,
-            consumed: bool,
+            length: usize,     // Number of chars after the marker in original string
+            count: usize,      // Number of times to repeat
+            index: usize,      // Index of marker in original string
+            end_index: usize,  // Range this marker applies to in original string
+            characters: usize, // Fully decompressed size this marker represents
+            consumed: bool,    // If this marker as be consumed by another marker
         }
 
         for compression in self.compressions.iter() {
@@ -185,17 +141,19 @@ impl Puzzle for Day09 {
             let mut repeats: Vec<Repeat> = vec![];
             let mut state = State::Normal;
             let mut marker = "".to_string();
-            let mut marker_index = 0;
-            let mut max_end_index = 0;
+            let mut marker_index = 0; // Start index of the current marker
+            let mut max_end_index = 0; // Max end index of any currently known markers
 
             for (i, character) in compression.chars().enumerate() {
                 match character {
                     '(' => {
+                        // Found a marker, start building its string
                         marker = "".to_string();
                         marker_index = i;
                         state = State::InMarker;
                     }
                     ')' => {
+                        // Done with the marker, parse out info and add it to list of markers
                         let re = Regex::new(r"(\d*)x(\d*)").unwrap();
                         let matches = re.captures(&marker).unwrap();
                         let length = find_val(matches.get(1).unwrap().as_str());
@@ -220,9 +178,13 @@ impl Puzzle for Day09 {
                         state = State::Normal;
                     }
                     _ => match state {
-                        State::InMarker => marker.push(character),
+                        State::InMarker => {
+                            // Add char to marker string
+                            marker.push(character)
+                        }
                         State::Normal => {
                             if i > max_end_index || max_end_index == 0 {
+                                // This char is not covered by a marker, so add it to output directly
                                 answer += 1;
                             }
                         }
@@ -230,11 +192,14 @@ impl Puzzle for Day09 {
                 }
             }
 
+            // Now loop through all markers in reverse direction and find overlaps.
+            // If a marker overlaps, then consume the child and add it into the parent.
             for i in (0..repeats.len()).rev() {
                 log::debug!("{} {:?}", i, repeats[i]);
                 let mut characters = 0;
                 for j in (i + 1)..repeats.len() {
                     if (repeats[j].index <= repeats[i].end_index) && !repeats[j].consumed {
+                        // Found an unconsumed child, add it in
                         characters += repeats[j].characters;
                         repeats[j].consumed = true;
                         log::debug!("    {} {:?}", j, repeats[j]);
@@ -242,6 +207,7 @@ impl Puzzle for Day09 {
                 }
 
                 if characters == 0 {
+                    // This marker has no child markers, so just use it on its own
                     characters = repeats[i].length
                 }
 
@@ -249,6 +215,7 @@ impl Puzzle for Day09 {
                 log::debug!("{} {:?}", i, repeats[i]);
             }
 
+            // Now add in the size of parent markers to the output
             for repeat in repeats.iter() {
                 if !repeat.consumed {
                     answer += repeat.characters;
