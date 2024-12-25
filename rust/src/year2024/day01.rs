2cde1b5226bddf1f2dfb6fb7035f1392c5f6e780
Mark Inderhees <mark.inderhees@gmail.com>
2024-12-24 22:09:43 -0800

    2024 day 1 part 1 done

--- rust/src/year2024/day01.rs
+++ rust/src/year2024/day01.rs
@@ -11,25 +11,47 @@ use crate::utils::utils::*;
 #[allow(unused_imports)]
 use std::collections::VecDeque;
 
-pub struct Day01 {}
+pub struct Day01 {
+    list1: Vec<u32>,
+    list2: Vec<u32>,
+}
 
 impl Puzzle for Day01 {
     #[allow(unused_variables)]
     fn from_input(input: &str) -> Result<Self> {
         #[allow(unused_mut)]
-        let mut day = Day01 {};
+        let mut day = Day01 {
+            list1: vec![],
+            list2: vec![],
+        };
+
+        for row in input.lines() {
+            let values: Vec<u32> = find_vals(row);
+            day.list1.push(values[0]);
+            day.list2.push(values[1]);
+        }
 
         Ok(day)
     }
 
     fn solve_part1(&mut self) -> Result<String> {
-        Ok("to do".to_string())
+        self.list1.sort();
+        self.list2.sort();
+        let mut distance = 0;
+        for (a, b) in self.list1.iter().zip(self.list2.iter()) {
+            if a > b {
+                distance += a - b;
+            } else {
+                distance += b - a;
+            }
+        }
+        Ok(distance.to_string())
     }
 
     fn answer_part1(&mut self, test: bool) -> Option<String> {
         match test {
-            true => None,
-            false => None,
+            true => Some(11.to_string()),
+            false => Some(1646452.to_string()),
         }
     }
 
